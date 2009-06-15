aras-p/glsl-optimizer

commit 3f2e006b759705abd7c409d30f9aeb1f2a75b83f

gallium: New pf_is_depth_and_stencil / pf_is_depth_or_stencil inlines.

src/gallium/include/pipe/p_format.h
@@ -549,11 +549,21 @@ pf_get_2d_size(const struct pipe_format_block *block, size_t stride, unsigned he
 }
 
 static INLINE boolean 
-pf_is_depth_stencil( enum pipe_format format )
+pf_is_depth_or_stencil( enum pipe_format format )
 {
    return (pf_get_component_bits( format, PIPE_FORMAT_COMP_Z ) +
            pf_get_component_bits( format, PIPE_FORMAT_COMP_S )) != 0;
 }
+
+static INLINE boolean 
+pf_is_depth_and_stencil( enum pipe_format format )
+{
+   return (pf_get_component_bits( format, PIPE_FORMAT_COMP_Z ) != 0 &&
+           pf_get_component_bits( format, PIPE_FORMAT_COMP_S ) != 0);
+}
+
+/** DEPRECATED: For backwards compatibility */
+#define pf_is_depth_stencil pf_is_depth_or_stencil
 
 static INLINE boolean 
 pf_is_compressed( enum pipe_format format )

projectceladon/external-mesa

commit 7946c91f5be44a32120a4ea4eac0a9d42a091be3

gallium: Fix segfault and valgrind error introduced with commit 3f2e006b759705abd7c409d30f9aeb1f2a75b83f
Signed-off-by: Thomas Hellstrom <thellstrom-at-vmware-dot-com>

src/gallium/include/pipe/p_format.h
@@ -563,7 +563,11 @@ pf_is_depth_and_stencil( enum pipe_format format )
 }
 
 /** DEPRECATED: For backwards compatibility */
-#define pf_is_depth_stencil pf_is_depth_or_stencil
+static INLINE boolean
+pf_is_depth_stencil( enum pipe_format format )
+{
+   return pf_is_depth_or_stencil( format );
+}
 
 static INLINE boolean 
 pf_is_compressed( enum pipe_format format )