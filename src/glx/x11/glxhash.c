aras-p/glsl-optimizer

commit 9666529b5a5be1fcde82caadc2fe2efa5ea81e49

glx: Use initstate_r / random_r instead of corrupting global random number state
Previously srandom and random were used.  This cause the global random
number generator state to be modified.  This caused problems for
applications that called srandom before calling into GLX.  By using
local state the global state is left unmodified.

This should fix bug #23774.

src/glx/x11/glxhash.c
@@ -87,9 +87,13 @@
 
 #define HASH_ALLOC malloc
 #define HASH_FREE  free
-#define HASH_RANDOM_DECL
-#define HASH_RANDOM_INIT(seed)  srandom(seed)
-#define HASH_RANDOM             random()
+#define HASH_RANDOM_DECL	struct random_data rd; int32_t rv; char rs[256]
+#define HASH_RANDOM_INIT(seed)					\
+   do {								\
+      (void) memset(&rd, 0, sizeof(rd));			\
+      (void) initstate_r(seed, rs, sizeof(rs), &rd);		\
+   } while(0)
+#define HASH_RANDOM             ((void) random_r(&rd, &rv), rv)
 #define HASH_RANDOM_DESTROY
 
 typedef struct __glxHashBucket

projectceladon/external-mesa

commit 58a981160e8b5c30479b070f470e40a6665b3cf7

Fix build on non GLIBC platforms (FreeBSD at least)
Build was broken by commit 9666529b5a5be1fcde82caadc2fe2efa5ea81e49

I'm not certain that this is entirely the correct fix since the demo
from bug #23774 seemed to work before the commit that broke the build.

Signed-off-by: Robert Noland <rnoland@2hip.net>
Signed-off-by: Brian Paul <brianp@vmware.com>

src/glx/x11/glxhash.c
@@ -88,6 +88,12 @@
 
 #define HASH_ALLOC malloc
 #define HASH_FREE  free
+#ifndef __GLIBC__
+#define HASH_RANDOM_DECL	char *ps, rs[256]
+#define HASH_RANDOM_INIT(seed)	ps = initstate(seed, rs, sizeof(rs))
+#define HASH_RANDOM		random()
+#define HASH_RANDOM_DESTROY	setstate(ps)
+#else
 #define HASH_RANDOM_DECL	struct random_data rd; int32_t rv; char rs[256]
 #define HASH_RANDOM_INIT(seed)					\
    do {								\
@@ -96,6 +102,7 @@
    } while(0)
 #define HASH_RANDOM             ((void) random_r(&rd, &rv), rv)
 #define HASH_RANDOM_DESTROY
+#endif
 
 typedef struct __glxHashBucket
 {