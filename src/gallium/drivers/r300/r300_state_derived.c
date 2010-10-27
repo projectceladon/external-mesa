aras-p/glsl-optimizer

commit 8449a4772a73f613d9425b691cffba6a261df813

r300g: fix texture border for 16-bits-per-channel formats
This is kinda hacky, but it's hard to come up with a generic solution for
all formats when only a few are used in practice (I mostly get B8G8R8*8).

src/gallium/drivers/r300/r300_state_derived.c
@@ -620,14 +620,19 @@ static uint32_t r300_get_border_color(enum pipe_format format,
             }
             break;
 
-        default:
-            /* I think the fat formats (16, 32) are specified
-             * as the 8-bit ones. I am not sure how compressed formats
-             * work here. */
+        case 8:
             r = ((float_to_ubyte(border_swizzled[0]) & 0xff) << 0) |
                 ((float_to_ubyte(border_swizzled[1]) & 0xff) << 8) |
                 ((float_to_ubyte(border_swizzled[2]) & 0xff) << 16) |
                 ((float_to_ubyte(border_swizzled[3]) & 0xff) << 24);
+            break;
+
+        case 16:
+            r = ((float_to_ubyte(border_swizzled[2]) & 0xff) << 0) |
+                ((float_to_ubyte(border_swizzled[1]) & 0xff) << 8) |
+                ((float_to_ubyte(border_swizzled[0]) & 0xff) << 16) |
+                ((float_to_ubyte(border_swizzled[3]) & 0xff) << 24);
+            break;
     }
 
     return r;

projectceladon/external-mesa

commit 3e6f7defdaf764ef0c4880025ba1cb465522648e

r300g: add a default channel ordering of texture border for unhandled formats
It should fix the texture border for compressed textures.
Broken since 8449a4772a73f613d9425b691cffba6a261df813.

src/gallium/drivers/r300/r300_state_derived.c
@@ -620,13 +620,6 @@ static uint32_t r300_get_border_color(enum pipe_format format,
             }
             break;
 
-        case 8:
-            r = ((float_to_ubyte(border_swizzled[0]) & 0xff) << 0) |
-                ((float_to_ubyte(border_swizzled[1]) & 0xff) << 8) |
-                ((float_to_ubyte(border_swizzled[2]) & 0xff) << 16) |
-                ((float_to_ubyte(border_swizzled[3]) & 0xff) << 24);
-            break;
-
         case 16:
             r = ((float_to_ubyte(border_swizzled[2]) & 0xff) << 0) |
                 ((float_to_ubyte(border_swizzled[1]) & 0xff) << 8) |
@@ -634,9 +627,12 @@ static uint32_t r300_get_border_color(enum pipe_format format,
                 ((float_to_ubyte(border_swizzled[3]) & 0xff) << 24);
             break;
 
+        case 8:
         default:
-            assert(0);
-            r = 0;
+            r = ((float_to_ubyte(border_swizzled[0]) & 0xff) << 0) |
+                ((float_to_ubyte(border_swizzled[1]) & 0xff) << 8) |
+                ((float_to_ubyte(border_swizzled[2]) & 0xff) << 16) |
+                ((float_to_ubyte(border_swizzled[3]) & 0xff) << 24);
             break;
     }
 