aras-p/glsl-optimizer

commit 5cbff0932e498f49b57cbb71037b93416bfe30e0

st/mesa: free the temporary bitmap/drawpix shader code
Fixes a per-shader memory leak when drawing glBitmaps, glDrawPixels
or glCopyPixels.

NOTE: This is a candidate for the 7.9 and 7.10 branches.

src/mesa/state_tracker/st_program.c
@@ -413,7 +413,7 @@ st_translate_fragment_program(struct st_context *st,
 #if FEATURE_drawpix
    if (key->bitmap) {
       /* glBitmap drawing */
-      struct gl_fragment_program *fp;
+      struct gl_fragment_program *fp; /* we free this temp program below */
 
       st_make_bitmap_fragment_program(st, &stfp->Base,
                                       &fp, &variant->bitmap_sampler);
@@ -423,7 +423,7 @@ st_translate_fragment_program(struct st_context *st,
    }
    else if (key->drawpixels) {
       /* glDrawPixels drawing */
-      struct gl_fragment_program *fp;
+      struct gl_fragment_program *fp; /* we free this temp program below */
 
       if (key->drawpixels_z || key->drawpixels_stencil) {
          fp = st_make_drawpix_z_stencil_program(st, key->drawpixels_z,
@@ -629,6 +629,14 @@ st_translate_fragment_program(struct st_context *st,
       tgsi_dump( stfp->tgsi.tokens, 0/*TGSI_DUMP_VERBOSE*/ );
       debug_printf("\n");
    }
+
+#if FEATURE_drawpix
+   if (key->bitmap || key->drawpixels) {
+      /* Free the temporary program made above */
+      struct gl_fragment_program *fp = &stfp->Base;
+      _mesa_reference_fragprog(st->ctx, &fp, NULL);
+   }
+#endif
 
    return variant;
 }

projectceladon/external-mesa

commit 01d9b4f9c97538b343f3a47e273a3d25388ec704

st/mesa: fix shader deletion regression
Fixes a regression from commit 5cbff0932e498f49b57cbb71037b93416bfe30e0.
The problem is *some* glDrawPixels fragment programs need to be deleted,
but not all.  Use an explicit flag to indicate whether or not the program
needs to be deleted.

This should fix http://bugs.freedesktop.org/show_bug.cgi?id=34049

src/mesa/state_tracker/st_program.c
@@ -406,6 +406,7 @@ st_translate_fragment_program(struct st_context *st,
 {
    struct pipe_context *pipe = st->pipe;
    struct st_fp_variant *variant = CALLOC_STRUCT(st_fp_variant);
+   GLboolean deleteFP = GL_FALSE;
 
    if (!variant)
       return NULL;
@@ -422,6 +423,7 @@ st_translate_fragment_program(struct st_context *st,
 
       variant->parameters = _mesa_clone_parameter_list(fp->Base.Parameters);
       stfp = st_fragment_program(fp);
+      deleteFP = GL_TRUE;
    }
    else if (key->drawpixels) {
       /* glDrawPixels drawing */
@@ -435,6 +437,7 @@ st_translate_fragment_program(struct st_context *st,
          /* RGBA */
          st_make_drawpix_fragment_program(st, &stfp->Base, &fp);
          variant->parameters = _mesa_clone_parameter_list(fp->Base.Parameters);
+         deleteFP = GL_TRUE;
       }
       stfp = st_fragment_program(fp);
    }
@@ -632,13 +635,11 @@ st_translate_fragment_program(struct st_context *st,
       debug_printf("\n");
    }
 
-#if FEATURE_drawpix
-   if (key->bitmap || key->drawpixels) {
+   if (deleteFP) {
       /* Free the temporary program made above */
       struct gl_fragment_program *fp = &stfp->Base;
       _mesa_reference_fragprog(st->ctx, &fp, NULL);
    }
-#endif
 
    return variant;
 }