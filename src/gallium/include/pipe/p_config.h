aras-p/glsl-optimizer

commit 7004582c1894ede839c44e292b413fe4916d7e9e

gallium: implement tests for PPC/PPC64

src/gallium/include/pipe/p_config.h
@@ -93,8 +93,11 @@
 #endif
 #endif
 
-#if 0 /* FIXME */
+#if defined(__PPC__)
 #define PIPE_ARCH_PPC
+#if defined(__PPC64__)
+#define PIPE_ARCH_PPC_64
+#endif
 #endif
 
 

projectceladon/external-mesa

commit 6ea839a9f26d1596c5fe3b62ba5aee8094e06c06

Fix PPC detection on darwin
Fixes regression introduced by 7004582c1894ede839c44e292b413fe4916d7e9e

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

src/gallium/include/pipe/p_config.h
@@ -99,9 +99,9 @@
 #endif
 #endif
 
-#if defined(__PPC__)
+#if defined(__ppc__) || defined(__ppc64__) || defined(__PPC__)
 #define PIPE_ARCH_PPC
-#if defined(__PPC64__)
+#if defined(__ppc64__) || defined(__PPC64__)
 #define PIPE_ARCH_PPC_64
 #endif
 #endif