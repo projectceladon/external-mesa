aras-p/glsl-optimizer

commit 34980cd153050111edc545545ddff11f5b68347e

nv50: don't drop flags definition when merging SAT with ADD/MAD

src/gallium/drivers/nv50/nv50_pc_optimize.c
@@ -462,6 +462,8 @@ nv_pass_lower_mods(struct nv_pass *ctx, struct nv_basic_block *b)
          mi->saturate = 1;
          mi->def[0] = nvi->def[0];
          mi->def[0]->insn = mi;
+         mi->flags_def = nvi->flags_def;
+         mi->flags_def->insn = mi;
          nv_nvi_delete(nvi);
       }
    }

projectceladon/external-mesa

commit 0fc2ab0d20d441c0cfe9e41138c91c2c54ee4fe6

nv50: fix potential null deref in insn modifer optimization pass
Bug introduced in 34980cd153050111edc545545ddff11f5b68347e.

src/gallium/drivers/nv50/nv50_pc_optimize.c
@@ -462,8 +462,10 @@ nv_pass_lower_mods(struct nv_pass *ctx, struct nv_basic_block *b)
          mi->saturate = 1;
          mi->def[0] = nvi->def[0];
          mi->def[0]->insn = mi;
-         mi->flags_def = nvi->flags_def;
-         mi->flags_def->insn = mi;
+         if (nvi->flags_def) {
+            mi->flags_def = nvi->flags_def;
+            mi->flags_def->insn = mi;
+         }
          nv_nvi_delete(nvi);
       }
    }