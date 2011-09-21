aras-p/glsl-optimizer

commit 99c67f27d35a4bbbbefada8117d5972c7583cf42

vega: implement handler/pointer conversion using a hash table
Before, we were just casting between 32-bit VGHandles and 64-bit pointers.

src/gallium/state_trackers/vega/image.c
@@ -349,6 +349,8 @@ void image_destroy(struct vg_image *img)
       array_destroy(img->children_array);
    }
 
+   vg_free_object(&img->base);
+
    pipe_sampler_view_reference(&img->sampler_view, NULL);
    FREE(img);
 }

src/gallium/state_trackers/vega/vg_context.c
@@ -183,6 +183,15 @@ void vg_init_object(struct vg_object *obj, struct vg_context *ctx, enum vg_objec
 {
    obj->type = type;
    obj->ctx = ctx;
+   obj->handle = create_handle(obj);
+}
+
+/** free object resources, but not the object itself */
+void vg_free_object(struct vg_object *obj)
+{
+   obj->type = 0;
+   obj->ctx = NULL;
+   destroy_handle(obj->handle);
 }
 
 VGboolean vg_context_is_object_valid(struct vg_context *ctx,
@@ -416,7 +425,7 @@ void vg_validate_state(struct vg_context *ctx)
 VGboolean vg_object_is_valid(VGHandle object, enum vg_object_type type)
 {
    struct vg_object *obj = handle_to_object(object);
-   if (object && is_aligned(obj) && obj->type == type)
+   if (obj && is_aligned(obj) && obj->type == type)
       return VG_TRUE;
    else
       return VG_FALSE;

src/gallium/state_trackers/vega/vg_context.h
@@ -129,11 +129,20 @@ struct vg_context
    struct blit_state *blit;
 };
 
+
+/**
+ *  Base class for VG objects like paths, images, fonts.
+ */
 struct vg_object {
    enum vg_object_type type;
+   VGHandle handle;
    struct vg_context *ctx;
 };
+
+
 void vg_init_object(struct vg_object *obj, struct vg_context *ctx, enum vg_object_type type);
+void vg_free_object(struct vg_object *obj);
+
 VGboolean vg_object_is_valid(VGHandle object, enum vg_object_type type);
 
 struct vg_context *vg_create_context(struct pipe_context *pipe,

src/gallium/state_trackers/vega/Makefile
@@ -26,6 +26,7 @@ C_SOURCES = \
            api_transform.c \
            arc.c \
            bezier.c \
+           handle.c \
            image.c \
            mask.c \
            paint.c \

src/gallium/state_trackers/vega/SConscript
@@ -28,6 +28,7 @@ vega_sources = [
     'api_transform.c',
     'arc.c',
     'bezier.c',
+    'handle.c',
     'image.c',
     'mask.c',
     'paint.c',

src/gallium/state_trackers/vega/api_path.c
@@ -170,7 +170,7 @@ void vegaAppendPathData(VGPath dstPath,
 
    p = handle_to_path(dstPath);
 
-   if (!pathData || !is_aligned_to(pathData, path_datatype_size(p))) {
+   if (!p || !is_aligned_to(p, path_datatype_size(p))) {
       vg_set_error(ctx, VG_ILLEGAL_ARGUMENT_ERROR);
       return;
    }

src/gallium/state_trackers/vega/handle.c
@@ -1,0 +1,99 @@
+/**************************************************************************
+ *
+ * Copyright 2010 VMware, Inc.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL VMWARE AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **************************************************************************/
+
+
+#include "handle.h"
+#include "util/u_hash.h"
+#include "util/u_hash_table.h"
+
+
+/**
+ * Hash keys are 32-bit VGHandles
+ */
+
+struct util_hash_table *handle_hash = NULL;
+
+
+static unsigned next_handle = 1;
+
+
+static unsigned
+hash_func(void *key)
+{
+   /* XXX this kind of ugly */
+   intptr_t ip = pointer_to_intptr(key);
+   return (unsigned) (ip & 0xffffffff);
+}
+
+
+static int
+compare(void *key1, void *key2)
+{
+   if (key1 < key2)
+      return -1;
+   else if (key1 > key2)
+      return +1;
+   else
+      return 0;
+}
+
+
+void
+init_handles(void)
+{
+   if (!handle_hash)
+      handle_hash = util_hash_table_create(hash_func, compare);
+}
+
+
+void
+free_handles(void)
+{
+   /* XXX destroy */
+}
+
+
+VGHandle
+create_handle(void *object)
+{
+   VGHandle h = next_handle++;
+   util_hash_table_set(handle_hash, intptr_to_pointer(h), object);
+#if DEBUG
+   {
+      void *v = handle_to_object(h);
+      assert(v == object);
+   }
+#endif
+   return h;
+}
+
+
+void
+destroy_handle(VGHandle h)
+{
+   util_hash_table_remove(handle_hash, intptr_to_pointer(h));
+}
+

src/gallium/state_trackers/vega/handle.h
@@ -34,8 +34,15 @@
 #ifndef HANDLE_H
 #define HANDLE_H
 
-#include "VG/openvg.h"
 #include "pipe/p_compiler.h"
+#include "util/u_hash_table.h"
+#include "util/u_pointer.h"
+
+#include "VG/openvg.h"
+#include "vg_context.h"
+
+
+extern struct util_hash_table *handle_hash;
 
 
 struct vg_mask_layer;
@@ -45,43 +52,77 @@ struct vg_paint;
 struct path;
 
 
+extern void
+init_handles(void);
+
+
+extern void
+free_handles(void);
+
+
+extern VGHandle
+create_handle(void *object);
+
+
+extern void
+destroy_handle(VGHandle h);
+
+
+static INLINE VGHandle
+object_to_handle(struct vg_object *obj)
+{
+   return obj ? obj->handle : VG_INVALID_HANDLE;
+}
+
+
 static INLINE VGHandle
 image_to_handle(struct vg_image *img)
 {
-   return (VGHandle) img;
+   /* vg_image is derived from vg_object */
+   return object_to_handle((struct vg_object *) img);
 }
 
 
 static INLINE VGHandle
 masklayer_to_handle(struct vg_mask_layer *mask)
 {
-   return (VGHandle) mask;
+   /* vg_object is derived from vg_object */
+   return object_to_handle((struct vg_object *) mask);
 }
 
 
 static INLINE VGHandle
 font_to_handle(struct vg_font *font)
 {
-   return (VGHandle) font;
+   return object_to_handle((struct vg_object *) font);
 }
+
 
 static INLINE VGHandle
 paint_to_handle(struct vg_paint *paint)
 {
-   return (VGHandle) paint;
+   return object_to_handle((struct vg_object *) paint);
 }
+
 
 static INLINE VGHandle
 path_to_handle(struct path *path)
 {
-   return (VGHandle) path;
+   return object_to_handle((struct vg_object *) path);
 }
 
 
 static INLINE void *
 handle_to_pointer(VGHandle h)
 {
-   return (void *) h;
+   void *v = util_hash_table_get(handle_hash, intptr_to_pointer(h));
+#ifdef DEBUG
+   if (v) {
+      struct vg_object *obj = (struct vg_object *) v;
+      assert(obj->handle == h);
+   }
+#endif
+   return v;
 }
 
 

projectceladon/external-mesa

commit 9b9f0bb610aa3497f0fbede110b35182e23eb983

st/vega: fix vg_context_is_object_valid()
vg_context_is_object_valid() checks if a handle is valid by checking if
the handle is a valid key of the object hash table.  However, the keys
of the object hash table were object pointers.

Fix vg_context_add_object() to use the handles as the keys so that
vg_context_is_object_valid() works.  This bug was introduced by
99c67f27d35a4bbbbefada8117d5972c7583cf42.

src/gallium/state_trackers/vega/image.c
@@ -287,7 +287,7 @@ struct vg_image * image_create(VGImageFormat format,
 
    image->sampler_view = view;
 
-   vg_context_add_object(ctx, VG_OBJECT_IMAGE, image);
+   vg_context_add_object(ctx, &image->base);
 
    image_cleari(image, 0, 0, 0, image->width, image->height);
    return image;
@@ -296,7 +296,7 @@ struct vg_image * image_create(VGImageFormat format,
 void image_destroy(struct vg_image *img)
 {
    struct vg_context *ctx = vg_current_context();
-   vg_context_remove_object(ctx, VG_OBJECT_IMAGE, img);
+   vg_context_remove_object(ctx, &img->base);
 
 
    if (img->parent) {
@@ -502,7 +502,7 @@ struct vg_image * image_child_image(struct vg_image *parent,
    array_append_data(parent->children_array,
                      &image, 1);
 
-   vg_context_add_object(ctx, VG_OBJECT_IMAGE, image);
+   vg_context_add_object(ctx, &image->base);
 
    return image;
 }

src/gallium/state_trackers/vega/mask.c
@@ -365,7 +365,7 @@ struct vg_mask_layer * mask_layer_create(VGint width, VGint height)
       mask->sampler_view = view;
    }
 
-   vg_context_add_object(ctx, VG_OBJECT_MASK, mask);
+   vg_context_add_object(ctx, &mask->base);
 
    return mask;
 }
@@ -374,7 +374,7 @@ void mask_layer_destroy(struct vg_mask_layer *layer)
 {
    struct vg_context *ctx = vg_current_context();
 
-   vg_context_remove_object(ctx, VG_OBJECT_MASK, layer);
+   vg_context_remove_object(ctx, &layer->base);
    pipe_sampler_view_reference(&layer->sampler_view, NULL);
    FREE(layer);
 }

src/gallium/state_trackers/vega/paint.c
@@ -199,7 +199,7 @@ struct vg_paint * paint_create(struct vg_context *ctx)
    const VGfloat def_ling[] = {0.0f, 0.0f, 1.0f, 0.0f};
    const VGfloat def_radg[] = {0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
    vg_init_object(&paint->base, ctx, VG_OBJECT_PAINT);
-   vg_context_add_object(ctx, VG_OBJECT_PAINT, paint);
+   vg_context_add_object(ctx, &paint->base);
 
    paint->type = VG_PAINT_TYPE_COLOR;
    memcpy(paint->solid.color, default_color,
@@ -230,7 +230,7 @@ void paint_destroy(struct vg_paint *paint)
    if (paint->pattern.sampler_view)
       pipe_sampler_view_reference(&paint->pattern.sampler_view, NULL);
    if (ctx)
-      vg_context_remove_object(ctx, VG_OBJECT_PAINT, paint);
+      vg_context_remove_object(ctx, &paint->base);
 
    free(paint->gradient.ramp_stopsi);
    free(paint->gradient.ramp_stops);

src/gallium/state_trackers/vega/path.c
@@ -192,7 +192,7 @@ struct path * path_create(VGPathDatatype dt, VGfloat scale, VGfloat bias,
 
    vg_init_object(&path->base, vg_current_context(), VG_OBJECT_PATH);
    path->caps = capabilities & VG_PATH_CAPABILITY_ALL;
-   vg_context_add_object(vg_current_context(), VG_OBJECT_PATH, path);
+   vg_context_add_object(vg_current_context(), &path->base);
 
    path->datatype = dt;
    path->scale = scale;
@@ -224,7 +224,7 @@ static void polygon_array_cleanup(struct polygon_array *polyarray)
 
 void path_destroy(struct path *p)
 {
-   vg_context_remove_object(vg_current_context(), VG_OBJECT_PATH, p);
+   vg_context_remove_object(vg_current_context(), &p->base);
 
    array_destroy(p->segments);
    array_destroy(p->control_points);

src/gallium/state_trackers/vega/text.c
@@ -138,7 +138,7 @@ struct vg_font *font_create(VGint glyphCapacityHint)
    vg_init_object(&font->base, ctx, VG_OBJECT_FONT);
    font->glyphs = cso_hash_create();
 
-   vg_context_add_object(ctx, VG_OBJECT_FONT, font);
+   vg_context_add_object(ctx, &font->base);
 
    return font;
 }
@@ -148,7 +148,7 @@ void font_destroy(struct vg_font *font)
    struct vg_context *ctx = vg_current_context();
    struct cso_hash_iter iter;
 
-   vg_context_remove_object(ctx, VG_OBJECT_FONT, font);
+   vg_context_remove_object(ctx, &font->base);
 
    iter = cso_hash_first_node(font->glyphs);
    while (!cso_hash_iter_is_null(iter)) {

src/gallium/state_trackers/vega/vg_context.c
@@ -196,38 +196,36 @@ void vg_free_object(struct vg_object *obj)
 
 VGboolean vg_context_is_object_valid(struct vg_context *ctx,
                                 enum vg_object_type type,
-                                VGHandle object)
+                                VGHandle handle)
 {
     if (ctx) {
        struct cso_hash *hash = ctx->owned_objects[type];
        if (!hash)
           return VG_FALSE;
-       return cso_hash_contains(hash, (unsigned)(long)object);
+       return cso_hash_contains(hash, (unsigned) handle);
     }
     return VG_FALSE;
 }
 
 void vg_context_add_object(struct vg_context *ctx,
-                           enum vg_object_type type,
-                           void *ptr)
+                           struct vg_object *obj)
 {
     if (ctx) {
-       struct cso_hash *hash = ctx->owned_objects[type];
+       struct cso_hash *hash = ctx->owned_objects[obj->type];
        if (!hash)
           return;
-       cso_hash_insert(hash, (unsigned)(long)ptr, ptr);
+       cso_hash_insert(hash, (unsigned) obj->handle, obj);
     }
 }
 
 void vg_context_remove_object(struct vg_context *ctx,
-                              enum vg_object_type type,
-                              void *ptr)
+                              struct vg_object *obj)
 {
    if (ctx) {
-      struct cso_hash *hash = ctx->owned_objects[type];
+      struct cso_hash *hash = ctx->owned_objects[obj->type];
       if (!hash)
          return;
-      cso_hash_take(hash, (unsigned)(long)ptr);
+      cso_hash_take(hash, (unsigned) obj->handle);
    }
 }
 

src/gallium/state_trackers/vega/vg_context.h
@@ -161,11 +161,9 @@ VGboolean vg_context_is_object_valid(struct vg_context *ctx,
                                      enum vg_object_type type,
                                      VGHandle object);
 void vg_context_add_object(struct vg_context *ctx,
-                           enum vg_object_type type,
-                           void *ptr);
+                           struct vg_object *obj);
 void vg_context_remove_object(struct vg_context *ctx,
-                              enum vg_object_type type,
-                              void *ptr);
+                              struct vg_object *obj);
 
 void vg_validate_state(struct vg_context *ctx);
 