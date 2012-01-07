aras-p/glsl-optimizer

commit 07ee9f374f2946f852896e9264c7fa83eafc3f16

i965/vs: Properly clear cur_value when propagating direct copies.
Consider the following code:

MOV A.x, B.x
MOV B.x, C.x

After the first line, cur_value[A][0] == B, indicating that A.x's
current value came from register B.

When processing the second line, we update cur_value[B][0] to C.
However, for drect copies, we fail to reset cur_value[A][0] to NULL.
This is necessary because the value of A is no longer the value of B.

Fixes Counter-Strike: Source in Wine (where the menu rendered completely
black in DX9 mode), completely white textures in Civilization V, and the
new Piglit test glsl-vs-copy-propagation-1.shader_test.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=42032
Tested-by: Matt Turner <mattst88@gmail.com>
Tested-by: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>

src/mesa/drivers/dri/i965/brw_vec4_copy_propagation.cpp
@@ -297,30 +297,26 @@ vec4_visitor::opt_copy_propagation()
       }
 
       /* Track available source registers. */
-      if (is_direct_copy(inst)) {
-	 int reg = virtual_grf_reg_map[inst->dst.reg] + inst->dst.reg_offset;
-	 for (int i = 0; i < 4; i++) {
-	    if (inst->dst.writemask & (1 << i)) {
-	       cur_value[reg][i] = &inst->src[0];
-	    }
+      const int reg = virtual_grf_reg_map[inst->dst.reg] + inst->dst.reg_offset;
+
+      /* Update our destination's current channel values.  For a direct copy,
+       * the value is the newly propagated source.  Otherwise, we don't know
+       * the new value, so clear it.
+       */
+      bool direct_copy = is_direct_copy(inst);
+      for (int i = 0; i < 4; i++) {
+	 if (inst->dst.writemask & (1 << i)) {
+	    cur_value[reg][i] = direct_copy ? &inst->src[0] : NULL;
 	 }
-	 continue;
       }
 
-      /* For any updated channels, clear tracking of them as a source
-       * or destination.
+      /* Clear the records for any registers whose current value came from
+       * our destination's updated channels, as the two are no longer equal.
        */
       if (inst->dst.file == GRF) {
 	 if (inst->dst.reladdr)
 	    memset(cur_value, 0, sizeof(cur_value));
 	 else {
-	    int reg = virtual_grf_reg_map[inst->dst.reg] + inst->dst.reg_offset;
-
-	    for (int i = 0; i < 4; i++) {
-	       if (inst->dst.writemask & (1 << i))
-		  cur_value[reg][i] = NULL;
-	    }
-
 	    for (int i = 0; i < virtual_grf_reg_count; i++) {
 	       for (int j = 0; j < 4; j++) {
 		  if (inst->dst.writemask & (1 << j) &&

projectceladon/external-mesa

commit 124f0dfe1790de915173cfd5ae34a1639fc0c57c

i965/vs: Fix invalid array access in copy propagation.
Accessing virtual_grf_reg_map[inst->dst.reg] is invalid if
inst->dst.file != GRF.  Since is_direct_copy already implies a GRF
destination, we can just move the check earlier.

Fixes a regression in commit 07ee9f374f2946f852896e9264c7fa83eafc3f16.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=44302
Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/mesa/drivers/dri/i965/brw_vec4_copy_propagation.cpp
@@ -297,23 +297,24 @@ vec4_visitor::opt_copy_propagation()
       }
 
       /* Track available source registers. */
-      const int reg = virtual_grf_reg_map[inst->dst.reg] + inst->dst.reg_offset;
-
-      /* Update our destination's current channel values.  For a direct copy,
-       * the value is the newly propagated source.  Otherwise, we don't know
-       * the new value, so clear it.
-       */
-      bool direct_copy = is_direct_copy(inst);
-      for (int i = 0; i < 4; i++) {
-	 if (inst->dst.writemask & (1 << i)) {
-	    cur_value[reg][i] = direct_copy ? &inst->src[0] : NULL;
-	 }
-      }
-
-      /* Clear the records for any registers whose current value came from
-       * our destination's updated channels, as the two are no longer equal.
-       */
       if (inst->dst.file == GRF) {
+	 const int reg =
+	    virtual_grf_reg_map[inst->dst.reg] + inst->dst.reg_offset;
+
+	 /* Update our destination's current channel values.  For a direct copy,
+	  * the value is the newly propagated source.  Otherwise, we don't know
+	  * the new value, so clear it.
+	  */
+	 bool direct_copy = is_direct_copy(inst);
+	 for (int i = 0; i < 4; i++) {
+	    if (inst->dst.writemask & (1 << i)) {
+	       cur_value[reg][i] = direct_copy ? &inst->src[0] : NULL;
+	    }
+	 }
+
+	 /* Clear the records for any registers whose current value came from
+	  * our destination's updated channels, as the two are no longer equal.
+	  */
 	 if (inst->dst.reladdr)
 	    memset(cur_value, 0, sizeof(cur_value));
 	 else {