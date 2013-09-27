aras-p/glsl-optimizer

commit b2e327e08f8519da131dd382adcc99240d433404

mesa: Add core support for the GL_AMD_performance_monitor extension.
This provides an interface for applications (and OpenGL-based tools) to
access GPU performance counters.  Since the exact performance counters
available vary between vendors and hardware generations, the extension
provides an API the application can use to get the names, types, and
minimum/maximum values of all available counters.  Counters are also
organized into groups.

Applications create "performance monitor" objects, select the counters
they want to track, and Begin/End monitoring, much like OpenGL's query
API.  Multiple monitors can be in flight simultaneously.

v2: Pass ctx to all driver hooks (suggested by Christoph), and attempt
    to fix overallocation of bitsets (caught by Christoph).  Incomplete.

v3: Significantly rework core data structures.  Store counters in groups
    rather than in a global list.  Use their array index in the group's
    counter list as the ID rather than trying to store a globally unique
    counter ID.  Use bitsets for active counters within a group, and
    also track which groups are active so that's easy to query.

v4: Remove _mesa_ prefix on static functions; detect out of memory
    conditions in new_performance_monitor(); make BeginPerfMonitor hook
    return a boolean rather than setting m->Active or raising an error.
    Switch to GLuint/unsigned for NumGroups, NumCounters, and
    MaxActiveCounters (which also means switching a bunch of temporary
    variable types).  All suggested by Brian Paul.  Also, remove
    commented out code at the bottom of the block.  Finally, fix the
    dispatch sanity test (noticed by Ian Romanick).

Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Brian Paul <brianp@vmware.com> [v3]
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/mesa/main/mtypes.h
@@ -1778,6 +1778,89 @@ struct gl_transform_feedback_state
 
 
 /**
+ * A "performance monitor" as described in AMD_performance_monitor.
+ */
+struct gl_perf_monitor_object
+{
+   GLboolean Active;
+
+   /**
+    * A list of groups with currently active counters.
+    *
+    * ActiveGroups[g] == n if there are n counters active from group 'g'.
+    */
+   unsigned *ActiveGroups;
+
+   /**
+    * An array of bitsets, subscripted by group ID, then indexed by counter ID.
+    *
+    * Checking whether counter 'c' in group 'g' is active can be done via:
+    *
+    *    BITSET_TEST(ActiveCounters[g], c)
+    */
+   GLuint **ActiveCounters;
+};
+
+
+union gl_perf_monitor_counter_value
+{
+   float f;
+   uint64_t u64;
+   uint32_t u32;
+};
+
+
+struct gl_perf_monitor_counter
+{
+   /** Human readable name for the counter. */
+   const char *Name;
+
+   /**
+    * Data type of the counter.  Valid values are FLOAT, UNSIGNED_INT,
+    * UNSIGNED_INT64_AMD, and PERCENTAGE_AMD.
+    */
+   GLenum Type;
+
+   /** Minimum counter value. */
+   union gl_perf_monitor_counter_value Minimum;
+
+   /** Maximum counter value. */
+   union gl_perf_monitor_counter_value Maximum;
+};
+
+
+struct gl_perf_monitor_group
+{
+   /** Human readable name for the group. */
+   const char *Name;
+
+   /**
+    * Maximum number of counters in this group which can be active at the
+    * same time.
+    */
+   GLuint MaxActiveCounters;
+
+   /** Array of counters within this group. */
+   const struct gl_perf_monitor_counter *Counters;
+   GLuint NumCounters;
+};
+
+
+/**
+ * Context state for AMD_performance_monitor.
+ */
+struct gl_perf_monitor_state
+{
+   /** Array of performance monitor groups (indexed by group ID) */
+   const struct gl_perf_monitor_group *Groups;
+   GLuint NumGroups;
+
+   /** The table of all performance monitors. */
+   struct _mesa_HashTable *Monitors;
+};
+
+
+/**
  * Names of the various vertex/fragment program register files, etc.
  *
  * NOTE: first four tokens must fit into 2 bits (see t_vb_arbprogram.c)
@@ -3176,6 +3259,7 @@ struct gl_extensions
    GLboolean EXT_vertex_array_bgra;
    GLboolean OES_standard_derivatives;
    /* vendor extensions */
+   GLboolean AMD_performance_monitor;
    GLboolean AMD_seamless_cubemap_per_texture;
    GLboolean AMD_vertex_shader_layer;
    GLboolean APPLE_object_purgeable;
@@ -3638,6 +3722,8 @@ struct gl_context
    struct gl_query_state Query;  /**< occlusion, timer queries */
 
    struct gl_transform_feedback_state TransformFeedback;
+
+   struct gl_perf_monitor_state PerfMonitor;
 
    struct gl_buffer_object *CopyReadBuffer; /**< GL_ARB_copy_buffer */
    struct gl_buffer_object *CopyWriteBuffer; /**< GL_ARB_copy_buffer */

src/mapi/glapi/gen/AMD_performance_monitor.xml
@@ -1,0 +1,87 @@
+<?xml version="1.0"?>
+<!DOCTYPE OpenGLAPI SYSTEM "gl_API.dtd">
+
+<OpenGLAPI>
+
+<category name="GL_AMD_performance_monitor" number="360">
+
+    <function name="GetPerfMonitorGroupsAMD" offset="assign">
+        <param name="numGroups" type="GLint *"/>
+        <param name="groupsSize" type="GLsizei"/>
+        <param name="groups" type="GLuint *"/>
+    </function>
+
+    <function name="GetPerfMonitorCountersAMD" offset="assign">
+        <param name="group" type="GLuint"/>
+        <param name="numCounters" type="GLint *"/>
+        <param name="maxActiveCounters" type="GLint *"/>
+        <param name="countersSize" type="GLsizei"/>
+        <param name="counters" type="GLuint *"/>
+    </function>
+
+    <function name="GetPerfMonitorGroupStringAMD" offset="assign">
+        <param name="group" type="GLuint"/>
+        <param name="bufSize" type="GLsizei"/>
+        <param name="length" type="GLsizei *"/>
+        <param name="groupString" type="GLchar *"/>
+    </function>
+
+    <function name="GetPerfMonitorCounterStringAMD" offset="assign">
+        <param name="group" type="GLuint"/>
+        <param name="counter" type="GLuint"/>
+        <param name="bufSize" type="GLsizei"/>
+        <param name="length" type="GLsizei *"/>
+        <param name="counterString" type="GLchar *"/>
+    </function>
+
+    <function name="GetPerfMonitorCounterInfoAMD" offset="assign">
+        <param name="group" type="GLuint"/>
+        <param name="counter" type="GLuint"/>
+        <param name="pname" type="GLenum"/>
+        <param name="data" type="GLvoid *"/>
+    </function>
+
+    <function name="GenPerfMonitorsAMD" offset="assign">
+        <param name="n" type="GLsizei"/>
+        <param name="monitors" type="GLuint *"/>
+    </function>
+
+    <function name="DeletePerfMonitorsAMD" offset="assign">
+        <param name="n" type="GLsizei"/>
+        <param name="monitors" type="GLuint *"/>
+    </function>
+
+    <function name="SelectPerfMonitorCountersAMD" offset="assign">
+        <param name="monitor" type="GLuint"/>
+        <param name="enable" type="GLboolean"/>
+        <param name="group" type="GLuint"/>
+        <param name="numCounters" type="GLint"/>
+        <param name="counterList" type="GLuint *"/>
+    </function>
+
+    <function name="BeginPerfMonitorAMD" offset="assign">
+        <param name="monitor" type="GLuint"/>
+    </function>
+
+    <function name="EndPerfMonitorAMD" offset="assign">
+        <param name="monitor" type="GLuint"/>
+    </function>
+
+    <function name="GetPerfMonitorCounterDataAMD" offset="assign">
+        <param name="monitor" type="GLuint"/>
+        <param name="pname" type="GLenum"/>
+        <param name="dataSize" type="GLsizei"/>
+        <param name="data" type="GLuint *"/>
+        <param name="bytesWritten" type="GLint *"/>
+    </function>
+
+    <enum name="COUNTER_TYPE_AMD" value="0x8BC0"/>
+    <enum name="COUNTER_RANGE_AMD" value="0x8BC1"/>
+    <enum name="UNSIGNED_INT64_AMD" value="0x8BC2"/>
+    <enum name="PECENTAGE_AMD" value="0x8BC3"/>
+    <enum name="PERFMON_RESULT_AVAILABLE_AMD" value="0x8BC4"/>
+    <enum name="PERFMON_RESULT_SIZE_AMD" value="0x8BC5"/>
+    <enum name="PERFMON_RESULT_AMD" value="0x8BC6"/>
+</category>
+
+</OpenGLAPI>

src/mapi/glapi/gen/Makefile.am
@@ -115,6 +115,7 @@ API_XML = \
 	ARB_texture_storage.xml \
 	ARB_vertex_array_object.xml \
 	AMD_draw_buffers_blend.xml \
+	AMD_performance_monitor.xml \
 	ARB_vertex_type_2_10_10_10_rev.xml \
 	APPLE_object_purgeable.xml \
 	APPLE_vertex_array_object.xml \

src/mapi/glapi/gen/gl_API.xml
@@ -12890,6 +12890,8 @@
     <enum name="FRAMEBUFFER_SRGB_CAPABLE_EXT"      value="0x8DBA"/>
 </category>
 
+<xi:include href="AMD_performance_monitor.xml" xmlns:xi="http://www.w3.org/2001/XInclude"/>
+
 <category name="GL_APPLE_texture_range" number="367">
     <enum name="TEXTURE_STORAGE_HINT_APPLE" count="1" value="0x85BC">
         <size name="TexParameteriv"/>

src/mapi/glapi/gen/gl_genexec.py
@@ -82,6 +82,7 @@ header = """/**
 #include "main/matrix.h"
 #include "main/multisample.h"
 #include "main/objectlabel.h"
+#include "main/performance_monitor.h"
 #include "main/pixel.h"
 #include "main/pixelstore.h"
 #include "main/points.h"

src/mesa/Makefile.sources
@@ -65,6 +65,7 @@ MAIN_FILES = \
         $(SRCDIR)main/objectlabel.c \
 	$(SRCDIR)main/pack.c \
 	$(SRCDIR)main/pbo.c \
+	$(SRCDIR)main/performance_monitor.c \
 	$(SRCDIR)main/pixel.c \
 	$(SRCDIR)main/pixelstore.c \
 	$(SRCDIR)main/pixeltransfer.c \

src/mesa/SConscript
@@ -97,6 +97,7 @@ main_sources = [
     'main/objectlabel.c',
     'main/pack.c',
     'main/pbo.c',
+    'main/performance_monitor.c',
     'main/pixel.c',
     'main/pixelstore.c',
     'main/pixeltransfer.c',

src/mesa/main/context.c
@@ -105,6 +105,7 @@
 #include "macros.h"
 #include "matrix.h"
 #include "multisample.h"
+#include "performance_monitor.h"
 #include "pixel.h"
 #include "pixelstore.h"
 #include "points.h"
@@ -764,6 +765,7 @@ init_attrib_groups(struct gl_context *ctx)
    _mesa_init_lighting( ctx );
    _mesa_init_matrix( ctx );
    _mesa_init_multisample( ctx );
+   _mesa_init_performance_monitors( ctx );
    _mesa_init_pixel( ctx );
    _mesa_init_pixelstore( ctx );
    _mesa_init_point( ctx );

projectceladon/external-mesa

commit 30e83b96d244c2da8ca71e6890dd5333a63b21c8

mesa: Include stdint.h in mtypes.h for uint32_t symbol.
This patch fixes the MSVC build error introduced with commit
b2e327e08f8519da131dd382adcc99240d433404.

api_arrayelt.c
src\mesa\main/mtypes.h(1809) : error C2061: syntax error : identifier 'uint32_t'
src\mesa\main/mtypes.h(1810) : error C2059: syntax error : '}'
src\mesa\main/mtypes.h(1825) : error C2079: 'Minimum' uses undefined union 'gl_perf_monitor_counter_value'
src\mesa\main/mtypes.h(1828) : error C2079: 'Maximum' uses undefined union 'gl_perf_monitor_counter_value'

Signed-off-by: Vinson Lee <vlee@freedesktop.org>

src/mesa/main/mtypes.h
@@ -34,6 +34,8 @@
 #define MTYPES_H
 
 
+#include <stdint.h>             /* uint32_t */
+
 #include "main/glheader.h"
 #include "main/config.h"
 #include "glapi/glapi.h"