aras-p/glsl-optimizer

commit 80964226e9b8a05c39157f9305c06c0b2861e080

implement NV_vdpau_interop v7
v2: Actually implement interop between the gallium
    state tracker and the VDPAU backend.

v3: Make it also available in non legacy contexts,
    fix video buffer sharing.

v4: deny interop if we don't have the same screen object

v5: rebased on upstream changes

v6: implemented VDPAUGetSurfaceivNV, improved error handling,
    unregister all surfaces in VDPAUFiniNV

v7: squash merge with Mareks changes

Signed-off-by: Christian König <christian.koenig@amd.com>

src/gallium/include/state_tracker/vdpau_interop.h
@@ -1,0 +1,49 @@
+/**************************************************************************
+ *
+ * Copyright 2013 Advanced Micro Devices, Inc.
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **************************************************************************/
+
+/*
+ * Authors:
+ *      Christian König <christian.koenig@amd.com>
+ *
+ */
+
+#ifndef _VDPAU_INTEROP_H_
+#define _VDPAU_INTEROP_H_
+
+/* driver specific functions for NV_vdpau_interop */
+
+#define VDP_FUNC_ID_BASE_DRIVER 0x2000
+#define VDP_FUNC_ID_VIDEO_SURFACE_GALLIUM (VDP_FUNC_ID_BASE_DRIVER + 0)
+#define VDP_FUNC_ID_OUTPUT_SURFACE_GALLIUM (VDP_FUNC_ID_BASE_DRIVER + 1)
+
+struct pipe_resource;
+struct pipe_video_buffer;
+
+typedef struct pipe_video_buffer *VdpVideoSurfaceGallium(uint32_t surface);
+typedef struct pipe_resource *VdpOutputSurfaceGallium(uint32_t surface);
+
+#endif /* _VDPAU_INTEROP_H_ */

src/gallium/state_trackers/vdpau/ftab.c
@@ -26,6 +26,9 @@
  **************************************************************************/
 
 #include <assert.h>
+
+#include "util/u_memory.h"
+
 #include "vdpau_private.h"
 
 static void* ftab[67] =
@@ -104,19 +107,31 @@ static void* ftab_winsys[1] =
    &vlVdpPresentationQueueTargetCreateX11  /* VDP_FUNC_ID_PRESENTATION_QUEUE_TARGET_CREATE_X11 */
 };
 
+static void* ftab_driver[2] =
+{
+   &vlVdpVideoSurfaceGallium, /* VDP_FUNC_ID_SURFACE_GALLIUM */
+   &vlVdpOutputSurfaceGallium /* VDP_FUNC_ID_OUTPUT_SURFACE_GALLIUM */
+};
+
 boolean vlGetFuncFTAB(VdpFuncId function_id, void **func)
 {
    assert(func);
+   *func = NULL;
+
    if (function_id < VDP_FUNC_ID_BASE_WINSYS) {
-      if (function_id > 66)
-         return FALSE;
-      *func = ftab[function_id];
-   }
-   else {
+      if (function_id < Elements(ftab))
+         *func = ftab[function_id];
+
+   } else if (function_id < VDP_FUNC_ID_BASE_DRIVER) {
       function_id -= VDP_FUNC_ID_BASE_WINSYS;
-      if (function_id > 0)
-         return FALSE;
-      *func = ftab_winsys[function_id];
+      if (function_id < Elements(ftab_winsys))
+         *func = ftab_winsys[function_id];
+
+   } else {
+      function_id -= VDP_FUNC_ID_BASE_DRIVER;
+      if (function_id < Elements(ftab_driver))
+         *func = ftab_driver[function_id];
    }
+
    return *func != NULL;
 }

src/gallium/state_trackers/vdpau/output.c
@@ -725,3 +725,14 @@ vlVdpOutputSurfaceRenderBitmapSurface(VdpOutputSurface destination_surface,
 
    return VDP_STATUS_OK;
 }
+
+struct pipe_resource *vlVdpOutputSurfaceGallium(VdpOutputSurface surface)
+{
+   vlVdpOutputSurface *vlsurface;
+
+   vlsurface = vlGetDataHTAB(surface);
+   if (!vlsurface || !vlsurface->surface)
+      return NULL;
+
+   return vlsurface->surface->texture;
+}

src/gallium/state_trackers/vdpau/surface.c
@@ -469,3 +469,24 @@ vlVdpVideoSurfaceClear(vlVdpSurface *vlsurf)
    }
    pipe->flush(pipe, NULL, 0);
 }
+
+/**
+ * Interop to mesa state tracker
+ */
+struct pipe_video_buffer *vlVdpVideoSurfaceGallium(VdpVideoSurface surface)
+{
+   vlVdpSurface *p_surf = vlGetDataHTAB(surface);
+   if (!p_surf)
+      return NULL;
+
+   pipe_mutex_lock(p_surf->device->mutex);
+   if (p_surf->video_buffer == NULL) {
+      struct pipe_context *pipe = p_surf->device->context;
+
+      /* try to create a video buffer if we don't already have one */
+      p_surf->video_buffer = pipe->create_video_buffer(pipe, &p_surf->templat);
+   }
+   pipe_mutex_unlock(p_surf->device->mutex);
+
+   return p_surf->video_buffer;
+}

src/gallium/state_trackers/vdpau/vdpau_private.h
@@ -36,6 +36,8 @@
 #include "pipe/p_compiler.h"
 #include "pipe/p_video_codec.h"
 
+#include "state_tracker/vdpau_interop.h"
+
 #include "util/u_debug.h"
 #include "util/u_rect.h"
 #include "os/os_thread.h"
@@ -497,6 +499,10 @@ VdpVideoMixerGetParameterValues vlVdpVideoMixerGetParameterValues;
 VdpVideoMixerGetAttributeValues vlVdpVideoMixerGetAttributeValues;
 VdpVideoMixerDestroy vlVdpVideoMixerDestroy;
 VdpGenerateCSCMatrix vlVdpGenerateCSCMatrix;
+
+/* interop to mesa state tracker */
+VdpVideoSurfaceGallium vlVdpVideoSurfaceGallium;
+VdpOutputSurfaceGallium vlVdpOutputSurfaceGallium;
 
 #define VDPAU_OUT   0
 #define VDPAU_ERR   1

src/mapi/glapi/gen/Makefile.am
@@ -141,6 +141,7 @@ API_XML = \
 	NV_conditional_render.xml \
 	NV_primitive_restart.xml \
 	NV_texture_barrier.xml \
+	NV_vdpau_interop.xml \
 	OES_EGL_image.xml \
 	GL3x.xml
 

src/mapi/glapi/gen/NV_vdpau_interop.xml
@@ -1,0 +1,69 @@
+<?xml version="1.0"?>
+<!DOCTYPE OpenGLAPI SYSTEM "gl_API.dtd">
+
+<OpenGLAPI>
+
+<category name="GL_NV_vdpau_interop" number="396">
+
+    <function name="VDPAUInitNV" offset="assign">
+	<param name="vdpDevice" type="const GLvoid *"/>
+	<param name="getProcAddress" type="const GLvoid *"/>
+    </function>
+
+    <function name="VDPAUFiniNV" offset="assign"/>
+
+    <function name="VDPAURegisterVideoSurfaceNV" offset="assign">
+        <return type="GLintptr"/>
+	<param name="vdpSurface" type="const GLvoid *"/>
+	<param name="target" type="GLenum"/>
+	<param name="numTextureNames" type="GLsizei"/>
+	<param name="textureNames" type="const GLuint *"/>
+    </function>
+
+    <function name="VDPAURegisterOutputSurfaceNV" offset="assign">
+        <return type="GLintptr"/>
+	<param name="vdpSurface" type="const GLvoid *"/>
+	<param name="target" type="GLenum"/>
+	<param name="numTextureNames" type="GLsizei"/>
+	<param name="textureNames" type="const GLuint *"/>
+    </function>
+
+    <function name="VDPAUIsSurfaceNV" offset="assign">
+	<param name="surface" type="GLintptr"/>
+    </function>
+
+    <function name="VDPAUUnregisterSurfaceNV" offset="assign">
+	<param name="surface" type="GLintptr"/>
+    </function>
+
+    <function name="VDPAUGetSurfaceivNV" offset="assign">
+	<param name="surface" type="GLintptr"/>
+	<param name="pname" type="GLenum"/>
+	<param name="bufSize" type="GLsizei"/>
+	<param name="length" type="GLsizei *"/>
+	<param name="values" type="GLint *"/>
+    </function>
+
+    <function name="VDPAUSurfaceAccessNV" offset="assign">
+	<param name="surface" type="GLintptr"/>
+	<param name="access" type="GLenum"/>
+    </function>
+
+    <function name="VDPAUMapSurfacesNV" offset="assign">
+	<param name="numSurfaces" type="GLsizei"/>
+	<param name="surfaces" type="const GLintptr *"/>
+    </function>
+
+    <function name="VDPAUUnmapSurfacesNV" offset="assign">
+	<param name="numSurfaces" type="GLsizei"/>
+	<param name="surfaces" type="const GLintptr *"/>
+    </function>
+
+    <enum name="SURFACE_STATE_NV"      value="0x86EB"/>
+    <enum name="SURFACE_REGISTERED_NV" value="0x86FD"/>
+    <enum name="SURFACE_MAPPED_NV"     value="0x8700"/>
+    <enum name="WRITE_DISCARD_NV"      value="0x88BE"/>
+
+</category>
+
+</OpenGLAPI>

src/mapi/glapi/gen/gl_API.xml
@@ -13118,4 +13118,6 @@
 
 <xi:include href="EXT_transform_feedback.xml" xmlns:xi="http://www.w3.org/2001/XInclude"/>
 
+<xi:include href="NV_vdpau_interop.xml" xmlns:xi="http://www.w3.org/2001/XInclude"/>
+
 </OpenGLAPI>

projectceladon/external-mesa

commit 717f517972b4440f7fbf2a24aa5a5e2fb17547ac

mesa: Add GL_NV_vdpau_interop functions to dispatch_sanity.cpp.
Fixes 'make check' failures introduced with commit
80964226e9b8a05c39157f9305c06c0b2861e080.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=70900
Signed-off-by: Vinson Lee <vlee@freedesktop.org>

src/mesa/main/tests/dispatch_sanity.cpp
@@ -921,6 +921,18 @@ const struct function gl_core_functions_possible[] = {
    { "glEndPerfMonitorAMD", 11, -1 },
    { "glGetPerfMonitorCounterDataAMD", 11, -1 },
 
+   /* GL_NV_vdpau_interop */
+   { "glVDPAUInitNV", 11, -1 },
+   { "glVDPAUFiniNV", 11, -1 },
+   { "glVDPAURegisterVideoSurfaceNV", 11, -1 },
+   { "glVDPAURegisterOutputSurfaceNV", 11, -1 },
+   { "glVDPAUIsSurfaceNV", 11, -1 },
+   { "glVDPAUUnregisterSurfaceNV", 11, -1 },
+   { "glVDPAUGetSurfaceivNV", 11, -1 },
+   { "glVDPAUSurfaceAccessNV", 11, -1 },
+   { "glVDPAUMapSurfacesNV", 11, -1 },
+   { "glVDPAUUnmapSurfacesNV", 11, -1 },
+
    { NULL, 0, -1 }
 };
 