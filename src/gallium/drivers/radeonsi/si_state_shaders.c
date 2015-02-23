yuq/mesa-lima

commit a27b74819ad375e8c0bc88e13f42c951d2b5cd6a

radeonsi: small fix in SPI state
Cc: 10.5 10.4 <mesa-stable@lists.freedesktop.org>
Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -662,8 +662,10 @@ bcolor:
 		}
 
 		if (j == vsinfo->num_outputs) {
-			/* No corresponding output found, load defaults into input */
-			tmp |= S_028644_OFFSET(0x20);
+			/* No corresponding output found, load defaults into input.
+			 * Don't set any other bits.
+			 * (FLAT_SHADE=1 completely changes behavior) */
+			tmp = S_028644_OFFSET(0x20);
 		}
 
 		si_pm4_set_reg(pm4,

projectceladon/external-mesa

commit 45550e6517dd71bcab752a781fd45b73762b520f

radeonsi: fix point sprites
Broken by a27b74819ad375e8c0bc88e13f42c951d2b5cd6a.

This fix is critical and should be ported to stable ASAP.

Cc: 10.5 10.4 <mesa-stable@lists.freedesktop.org>

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -661,7 +661,7 @@ bcolor:
 			}
 		}
 
-		if (j == vsinfo->num_outputs) {
+		if (j == vsinfo->num_outputs & !G_028644_PT_SPRITE_TEX(tmp)) {
 			/* No corresponding output found, load defaults into input.
 			 * Don't set any other bits.
 			 * (FLAT_SHADE=1 completely changes behavior) */