yuq/mesa-lima

commit f00c5f85b82efe9535b18dbf97c4591fb28aeae6

glsl: support packing of arrays of arrays
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/glsl/lower_packed_varyings.cpp
@@ -645,14 +645,8 @@ lower_packed_varyings_visitor::needs_lowering(ir_variable *var)
    if (var->data.explicit_location)
       return false;
 
-   const glsl_type *type = var->type;
-   if (this->gs_input_vertices != 0) {
-      assert(type->is_array());
-      type = type->element_type();
-   }
-   if (type->is_array())
-      type = type->fields.array;
-   if (type->vector_elements == 4 && !type->is_double())
+   if (var->type->without_array()->vector_elements == 4 &&
+       !var->type->is_double())
       return false;
    return true;
 }

projectceladon/external-mesa

commit 84572316315af4774358f3eb574146b52e30fecd

glsl: fix packing support for arrays of doubles
Broke in commit f00c5f85b82efe9535b18dbf97c4591fb28aeae6 when
adding support for multidimensional arrays

Reviewed-by: Ilia Mirkin <imirkin at alum.mit.edu>

src/glsl/lower_packed_varyings.cpp
@@ -645,8 +645,8 @@ lower_packed_varyings_visitor::needs_lowering(ir_variable *var)
    if (var->data.explicit_location)
       return false;
 
-   if (var->type->without_array()->vector_elements == 4 &&
-       !var->type->is_double())
+   const glsl_type *type = var->type->without_array();
+   if (type->vector_elements == 4 && !type->is_double())
       return false;
    return true;
 }