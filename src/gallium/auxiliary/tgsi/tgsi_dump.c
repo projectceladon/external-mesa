victoredwardocallaghan/mesa-GLwork

tgsi: show textual format representation
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/gallium/auxiliary/tgsi/tgsi_dump.c
@@ -352,7 +352,7 @@ iter_declaration(
       TXT(", ");
       ENM(decl->Image.Resource, tgsi_texture_names);
       TXT(", ");
-      UID(decl->Image.Format);
+      TXT(util_format_short_name(decl->Image.Format));
       if (decl->Image.Writable)
          TXT(", WR");
       if (decl->Image.Raw)

projectceladon/external-mesa

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/gallium/auxiliary/tgsi/tgsi_dump.c
@@ -352,7 +352,7 @@ iter_declaration(
       TXT(", ");
       ENM(decl->Image.Resource, tgsi_texture_names);
       TXT(", ");
-      UID(decl->Image.Format);
+      TXT(util_format_name(decl->Image.Format));
       if (decl->Image.Writable)
          TXT(", WR");
       if (decl->Image.Raw)