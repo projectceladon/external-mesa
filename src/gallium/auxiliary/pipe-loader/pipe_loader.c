yuq/mesa-lima

commit 678dadf1237a3fb492ee2d8daa32d0205dea59ba

gallium: move driinfo XML to pipe_loader
We will switch to the pipe_loader loading the configuration options,
so that they can be passed to the driver independently of the state
tracker.

Put the description into its own file so that it can be merged easily
with driver-specific options in future commits.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -31,6 +31,7 @@
 #include "util/u_memory.h"
 #include "util/u_string.h"
 #include "util/u_dl.h"
+#include "util/xmlpool.h"
 
 #ifdef _MSC_VER
 #include <stdlib.h>
@@ -45,6 +46,10 @@ static int (*backends[])(struct pipe_loader_device **, int) = {
 #endif
    &pipe_loader_sw_probe
 };
+
+const char gallium_driinfo_xml[] =
+#include "driinfo_gallium.h"
+;
 
 int
 pipe_loader_probe(struct pipe_loader_device **devs, int ndev)

src/gallium/auxiliary/pipe-loader/Makefile.am
@@ -5,6 +5,7 @@ include $(top_srcdir)/src/gallium/Automake.inc
 AM_CFLAGS = \
 	-I$(top_srcdir)/src/loader \
 	-I$(top_srcdir)/src/gallium/winsys \
+	-I$(top_builddir)/src/util \
 	$(GALLIUM_PIPE_LOADER_DEFINES) \
 	$(GALLIUM_CFLAGS) \
 	$(VISIBILITY_CFLAGS)

src/gallium/auxiliary/pipe-loader/Makefile.sources
@@ -2,7 +2,8 @@ COMMON_SOURCES := \
 	pipe_loader.c \
 	pipe_loader.h \
 	pipe_loader_priv.h \
-	pipe_loader_sw.c
+	pipe_loader_sw.c \
+	driinfo_gallium.h
 
 DRM_SOURCES := \
 	pipe_loader_drm.c

src/gallium/auxiliary/pipe-loader/driinfo_gallium.h
@@ -1,0 +1,35 @@
+// DriConf options supported by all Gallium DRI drivers.
+DRI_CONF_SECTION_PERFORMANCE
+   DRI_CONF_MESA_GLTHREAD("false")
+   DRI_CONF_MESA_NO_ERROR("false")
+   DRI_CONF_DISABLE_EXT_BUFFER_AGE("false")
+   DRI_CONF_DISABLE_OML_SYNC_CONTROL("false")
+DRI_CONF_SECTION_END
+
+DRI_CONF_SECTION_QUALITY
+   DRI_CONF_FORCE_S3TC_ENABLE("false")
+   DRI_CONF_PP_CELSHADE(0)
+   DRI_CONF_PP_NORED(0)
+   DRI_CONF_PP_NOGREEN(0)
+   DRI_CONF_PP_NOBLUE(0)
+   DRI_CONF_PP_JIMENEZMLAA(0, 0, 32)
+   DRI_CONF_PP_JIMENEZMLAA_COLOR(0, 0, 32)
+DRI_CONF_SECTION_END
+
+DRI_CONF_SECTION_DEBUG
+   DRI_CONF_FORCE_GLSL_EXTENSIONS_WARN("false")
+   DRI_CONF_DISABLE_GLSL_LINE_CONTINUATIONS("false")
+   DRI_CONF_DISABLE_BLEND_FUNC_EXTENDED("false")
+   DRI_CONF_DISABLE_SHADER_BIT_ENCODING("false")
+   DRI_CONF_FORCE_GLSL_VERSION(0)
+   DRI_CONF_ALLOW_GLSL_EXTENSION_DIRECTIVE_MIDSHADER("false")
+   DRI_CONF_ALLOW_GLSL_BUILTIN_VARIABLE_REDECLARATION("false")
+   DRI_CONF_ALLOW_HIGHER_COMPAT_VERSION("false")
+   DRI_CONF_FORCE_GLSL_ABS_SQRT("false")
+   DRI_CONF_GLSL_CORRECT_DERIVATIVES_AFTER_DISCARD("false")
+DRI_CONF_SECTION_END
+
+DRI_CONF_SECTION_MISCELLANEOUS
+   DRI_CONF_ALWAYS_HAVE_DEPTH_BUFFER("false")
+   DRI_CONF_GLSL_ZERO_INIT("false")
+DRI_CONF_SECTION_END

src/gallium/auxiliary/pipe-loader/pipe_loader.h
@@ -183,6 +183,8 @@ pipe_loader_drm_probe(struct pipe_loader_device **devs, int ndev);
 bool
 pipe_loader_drm_probe_fd(struct pipe_loader_device **dev, int fd);
 
+extern const char gallium_driinfo_xml[];
+
 #ifdef __cplusplus
 }
 #endif

src/gallium/state_trackers/dri/dri_screen.c
@@ -30,7 +30,6 @@
  */
 
 #include "utils.h"
-#include "util/xmlpool.h"
 
 #include "dri_screen.h"
 #include "dri_context.h"
@@ -51,44 +50,7 @@
 
 const __DRIconfigOptionsExtension gallium_config_options = {
    .base = { __DRI_CONFIG_OPTIONS, 1 },
-   .xml =
-
-   DRI_CONF_BEGIN
-      DRI_CONF_SECTION_PERFORMANCE
-         DRI_CONF_MESA_GLTHREAD("false")
-         DRI_CONF_MESA_NO_ERROR("false")
-         DRI_CONF_DISABLE_EXT_BUFFER_AGE("false")
-         DRI_CONF_DISABLE_OML_SYNC_CONTROL("false")
-      DRI_CONF_SECTION_END
-
-      DRI_CONF_SECTION_QUALITY
-         DRI_CONF_FORCE_S3TC_ENABLE("false")
-         DRI_CONF_PP_CELSHADE(0)
-         DRI_CONF_PP_NORED(0)
-         DRI_CONF_PP_NOGREEN(0)
-         DRI_CONF_PP_NOBLUE(0)
-         DRI_CONF_PP_JIMENEZMLAA(0, 0, 32)
-         DRI_CONF_PP_JIMENEZMLAA_COLOR(0, 0, 32)
-      DRI_CONF_SECTION_END
-
-      DRI_CONF_SECTION_DEBUG
-         DRI_CONF_FORCE_GLSL_EXTENSIONS_WARN("false")
-         DRI_CONF_DISABLE_GLSL_LINE_CONTINUATIONS("false")
-         DRI_CONF_DISABLE_BLEND_FUNC_EXTENDED("false")
-         DRI_CONF_DISABLE_SHADER_BIT_ENCODING("false")
-         DRI_CONF_FORCE_GLSL_VERSION(0)
-         DRI_CONF_ALLOW_GLSL_EXTENSION_DIRECTIVE_MIDSHADER("false")
-         DRI_CONF_ALLOW_GLSL_BUILTIN_VARIABLE_REDECLARATION("false")
-         DRI_CONF_ALLOW_HIGHER_COMPAT_VERSION("false")
-         DRI_CONF_FORCE_GLSL_ABS_SQRT("false")
-         DRI_CONF_GLSL_CORRECT_DERIVATIVES_AFTER_DISCARD("false")
-      DRI_CONF_SECTION_END
-
-      DRI_CONF_SECTION_MISCELLANEOUS
-         DRI_CONF_ALWAYS_HAVE_DEPTH_BUFFER("false")
-         DRI_CONF_GLSL_ZERO_INIT("false")
-      DRI_CONF_SECTION_END
-   DRI_CONF_END
+   .xml = gallium_driinfo_xml
 };
 
 #define false 0

projectceladon/external-mesa

commit 2556a19ee3257fa7edf29e7fcdac5b43695a1c7f

pipe-loader: fix driinfo for software and non-radeonsi drivers
Fixes: 678dadf1237 ("gallium: move driinfo XML to pipe_loader")
Reviewed-by: Thomas Hellström <thellstrom@vmware.com>

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -51,7 +51,9 @@ static int (*backends[])(struct pipe_loader_device **, int) = {
 };
 
 const char gallium_driinfo_xml[] =
+   DRI_CONF_BEGIN
 #include "driinfo_gallium.h"
+   DRI_CONF_END
 ;
 
 int

src/gallium/state_trackers/dri/drisw.c
@@ -400,12 +400,12 @@ drisw_init_screen(__DRIscreen * sPriv)
    sPriv->driverPrivate = (void *)screen;
    sPriv->extensions = drisw_screen_extensions;
 
-   struct pipe_screen_config config;
+   if (pipe_loader_sw_probe_dri(&screen->dev, &drisw_lf)) {
+      struct pipe_screen_config config;
+      config.flags = dri_init_options_get_screen_flags(screen);
 
-   config.flags = dri_init_options_get_screen_flags(screen);
-
-   if (pipe_loader_sw_probe_dri(&screen->dev, &drisw_lf))
       pscreen = pipe_loader_create_screen(screen->dev, &config);
+   }
 
    if (!pscreen)
       goto fail;