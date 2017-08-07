yuq/mesa-lima

commit a6a6146aa91154fd31fb218bc60345bdda0d4f5d

radv: Don't allow fmask swizzling for shareable images.
Also adds an assert because you never know how the winsys changes, and
multiprocess format differences are annoying.

Fixes: 1e696b962b7 "radv: add separate fmask tile swizzle counter."
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_image.c
@@ -616,7 +616,8 @@ radv_image_get_fmask_info(struct radv_device *device,
 	info.samples = 1;
 	fmask.flags = image->surface.flags | RADEON_SURF_FMASK;
 
-	info.surf_index = &device->fmask_mrt_offset_counter;
+	if (!image->shareable);
+		info.surf_index = &device->fmask_mrt_offset_counter;
 
 	/* Force 2D tiling if it wasn't set. This may occur when creating
 	 * FMASK for MSAA resolve on R6xx. On R6xx, the single-sample
@@ -649,6 +650,8 @@ radv_image_get_fmask_info(struct radv_device *device,
 	out->tile_swizzle = fmask.tile_swizzle;
 	out->alignment = MAX2(256, fmask.surf_alignment);
 	out->size = fmask.surf_size;
+
+	assert(!out->tile_swizzle || !image->shareable);
 }
 
 static void

projectceladon/external-mesa

commit 363b488ed94795f7567bcc53016d5be5dfef4ad3

radv: remove semicolon in if(...);
Trivial.

Fixes: a6a6146aa91 "radv: Don't allow fmask swizzling for shareable images."

src/amd/vulkan/radv_image.c
@@ -616,7 +616,7 @@ radv_image_get_fmask_info(struct radv_device *device,
 	info.samples = 1;
 	fmask.flags = image->surface.flags | RADEON_SURF_FMASK;
 
-	if (!image->shareable);
+	if (!image->shareable)
 		info.surf_index = &device->fmask_mrt_offset_counter;
 
 	/* Force 2D tiling if it wasn't set. This may occur when creating