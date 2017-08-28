yuq/mesa-lima

commit c1ac1a3d257460c2706e91ff933679d7c8889294

i965: Add a brw_hw_type_to_reg_type() function
Will be used in later commits.

Reviewed-by: Scott D Phillips <scott.d.phillips@intel.com>

src/intel/compiler/brw_reg_type.c
@@ -70,6 +70,31 @@ brw_reg_type_to_hw_type(const struct gen_device_info *devinfo,
 }
 
 /**
+ * Convert the hardware representation into a brw_reg_type enumeration value.
+ *
+ * The hardware encoding may depend on whether the value is an immediate.
+ */
+enum brw_reg_type
+brw_hw_type_to_reg_type(const struct gen_device_info *devinfo,
+                        enum brw_reg_file file, unsigned hw_type)
+{
+   if (file == BRW_IMMEDIATE_VALUE) {
+      for (enum brw_reg_type i = 0; i <= BRW_REGISTER_TYPE_LAST; i++) {
+         if (gen4_hw_type[i].imm_type == hw_type) {
+            return i;
+         }
+      }
+   } else {
+      for (enum brw_reg_type i = 0; i <= BRW_REGISTER_TYPE_LAST; i++) {
+         if (gen4_hw_type[i].reg_type == hw_type) {
+            return i;
+         }
+      }
+   }
+   unreachable("not reached");
+}
+
+/**
  * Return the element size given a hardware register type and file.
  *
  * The hardware encoding may depend on whether the value is an immediate.

src/intel/compiler/brw_reg_type.h
@@ -63,6 +63,10 @@ unsigned
 brw_reg_type_to_hw_type(const struct gen_device_info *devinfo,
                         enum brw_reg_file file, enum brw_reg_type type);
 
+enum brw_reg_type
+brw_hw_type_to_reg_type(const struct gen_device_info *devinfo,
+                        enum brw_reg_file file, unsigned hw_type);
+
 #define brw_element_size(devinfo, inst, operand)                             \
    brw_hw_reg_type_to_size(devinfo,                                          \
                            brw_inst_ ## operand ## _reg_file(devinfo, inst), \

projectceladon/external-mesa

commit 44a76e0aca41267114589394ad17be44bfacfb1a

intel/compiler: Cast reg types explicitly
Makes coverity happier.

CID: 1416799
Fixes: c1ac1a3d25 (i965: Add a brw_hw_type_to_reg_type() function)

Reviewed-by: Matt Turner <mattst88@gmail.com>
Signed-off-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

src/intel/compiler/brw_reg_type.c
@@ -111,13 +111,13 @@ brw_hw_type_to_reg_type(const struct gen_device_info *devinfo,
 {
    if (file == BRW_IMMEDIATE_VALUE) {
       for (enum brw_reg_type i = 0; i <= BRW_REGISTER_TYPE_LAST; i++) {
-         if (gen4_hw_type[i].imm_type == hw_type) {
+         if (gen4_hw_type[i].imm_type == (enum hw_imm_type)hw_type) {
             return i;
          }
       }
    } else {
       for (enum brw_reg_type i = 0; i <= BRW_REGISTER_TYPE_LAST; i++) {
-         if (gen4_hw_type[i].reg_type == hw_type) {
+         if (gen4_hw_type[i].reg_type == (enum hw_reg_type)hw_type) {
             return i;
          }
       }