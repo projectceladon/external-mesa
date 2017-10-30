yuq/mesa-lima

commit 2c873060d3578c7004c0c3731a6f7c783c4e237f

i965: Delete unused brw_vs_prog_data::nr_attributes field.
Reviewed-by: Matt Turner <mattst88@gmail.com>

src/intel/compiler/brw_vec4.cpp
@@ -2829,7 +2829,6 @@ brw_compile_vs(const struct brw_compiler *compiler, void *log_data,
       prog_data->base.urb_read_length =
          DIV_ROUND_UP(MAX2(nr_attribute_slots, 1), 2);
 
-   prog_data->nr_attributes = nr_attributes;
    prog_data->nr_attribute_slots = nr_attribute_slots;
 
    /* Since vertex shaders reuse the same VUE entry for inputs and outputs

src/intel/compiler/brw_compiler.h
@@ -954,7 +954,6 @@ struct brw_vs_prog_data {
    GLbitfield64 inputs_read;
    GLbitfield64 double_inputs_read;
 
-   unsigned nr_attributes;
    unsigned nr_attribute_slots;
 
    bool uses_vertexid;

projectceladon/external-mesa

commit 71c591c0a061a0ea79f40cdded6930cac347dab0

i965: remove unused variable
Fixes: 2c873060d3578c7004c0 "i965: Delete unused
       brw_vs_prog_data::nr_attributes field."
Cc: Kenneth Graunke <kenneth@whitecape.org>
Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
Reviewed-by: Eduardo Lima Mitev <elima@igalia.com>

src/intel/compiler/brw_vec4.cpp
@@ -2815,9 +2815,6 @@ brw_compile_vs(const struct brw_compiler *compiler, void *log_data,
       nr_attribute_slots++;
    }
 
-   unsigned nr_attributes = nr_attribute_slots -
-      DIV_ROUND_UP(_mesa_bitcount_64(shader->info.double_inputs_read), 2);
-
    /* The 3DSTATE_VS documentation lists the lower bound on "Vertex URB Entry
     * Read Length" as 1 in vec4 mode, and 0 in SIMD8 mode.  Empirically, in
     * vec4 mode, the hardware appears to wedge unless we read something.