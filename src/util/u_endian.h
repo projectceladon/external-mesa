yuq/mesa-lima

commit 6a353479a7577dcff7c7a31809f27b59270648fb

util: Assume little endian in the absence of platform-specific handling

src/util/u_endian.h
@@ -67,4 +67,7 @@
 
 #endif
 
+#warn Unknown Endianness for this platform. Assuming little endian
+#define PIPE_ARCH_LITTLE_ENDIAN
+
 #endif

projectceladon/external-mesa

commit 60bae84d37fa83817847b353e0ae12ec8e47332e

util: Use preprocessor correctly
Fixes: 6a353479a757 ("util: Assume little endian in the absence of
                      platform-specific handling")

src/util/u_endian.h
@@ -67,7 +67,7 @@
 
 #endif
 
-#warn Unknown Endianness for this platform. Assuming little endian
+#warning Unknown Endianness for this platform. Assuming little endian
 #define PIPE_ARCH_LITTLE_ENDIAN
 
 #endif