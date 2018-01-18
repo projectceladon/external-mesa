yuq/mesa-lima

commit 8e4efdc895eacca931b94a0c1fa01aae5c34463b

vbo: optimize some display list drawing (v2)
The vbo_save_vertex_list structure records one or more glBegin/End
primitives which all have the same vertex format.

To draw these primitives, we setup the vertex array state, then
issue the drawing command.  Before, the 'start' vertex was typically
zero and we used the vertex array pointer to indicate where the
vertex data starts.

This patch checks if the vertex buffer offset is an exact multiple of
the vertex size.  If so, that means we can use zero-based vertex array
pointers and use the draw's start value to indicate where the vertex
data starts.

This means a series of display list drawing commands may have
identical vertex array state.  This will get filtered out by the
Gallium CSO module so we can issue a tight series of drawing commands
without state changes to the device.

Note that this also works for a series of glCallList commands (not
just one list that contains multiple glBegin/End pairs).

No Piglit or conform changes.

v2: minor fixes suggested by Ian.

Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/mesa/vbo/vbo_save_draw.c
@@ -146,6 +146,18 @@ bind_vertex_list(struct gl_context *ctx,
    memcpy(node_attrsz, node->attrsz, sizeof(node->attrsz));
    memcpy(node_attrtype, node->attrtype, sizeof(node->attrtype));
 
+   if (aligned_vertex_buffer_offset(node)) {
+      /* The vertex size is an exact multiple of the buffer offset.
+       * This means that we can use zero-based vertex attribute pointers
+       * and specify the start of the primitive with the _mesa_prim::start
+       * field.  This results in issuing several draw calls with identical
+       * vertex attribute information.  This can result in fewer state
+       * changes in drivers.  In particular, the Gallium CSO module will
+       * filter out redundant vertex buffer changes.
+       */
+      buffer_offset = 0;
+   }
+
    /* Install the default (ie Current) attributes first, then overlay
     * all active ones.
     */

src/mesa/vbo/vbo_save.h
@@ -86,6 +86,20 @@ struct vbo_save_vertex_list {
    struct vbo_save_primitive_store *prim_store;
 };
 
+
+/**
+ * Is the vertex lists's buffer offset an exact multiple of the
+ * vertex size (in bytes)?  This is used to check for a vertex array /
+ * drawing optimization.
+ */
+static inline bool
+aligned_vertex_buffer_offset(const struct vbo_save_vertex_list *node)
+{
+   unsigned vertex_size = node->vertex_size * sizeof(GLfloat); /* in bytes */
+   return vertex_size != 0 && node->buffer_offset % vertex_size == 0;
+}
+
+
 /* These buffers should be a reasonable size to support upload to
  * hardware.  Current vbo implementation will re-upload on any
  * changes, so don't make too big or apps which dynamically create

src/mesa/vbo/vbo_save_api.c
@@ -546,6 +546,20 @@ compile_vertex_list(struct gl_context *ctx)
       save->prim_store = alloc_prim_store();
    }
 
+   /*
+    * If the vertex buffer offset is a multiple of the vertex size,
+    * we can use the _mesa_prim::start value to indicate where the
+    * vertices starts, instead of the buffer offset.  Also see the
+    * bind_vertex_list() function.
+    */
+   if (aligned_vertex_buffer_offset(node)) {
+      const unsigned start_offset =
+         node->buffer_offset / (node->vertex_size * sizeof(GLfloat));
+      for (unsigned i = 0; i < save->prim_count; i++) {
+         save->prims[i].start += start_offset;
+      }
+   }
+
    /* Reset our structures for the next run of vertices:
     */
    reset_counters(ctx);

projectceladon/external-mesa

commit 58dea91e3512d7291c9ab0e1da8d27c586a3bdb0

vbo: fix VBO optimization regression
The optimization in change 8e4efdc895ea ("vbo: optimize some display
list drawing") missed the loopback case.  This is used when the
glBegin/End primitive doesn't have a uniform set of vertex attributes.
The new Piglit gl-1.0-dlist-materials test hits this.

So check the aligned_vertex_buffer_offset(list) value and adjust the
buffer offset accordingly.

We also need to remove the 'start == 0' assertion in the loopback
code since it no longer applies.

Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/mesa/vbo/vbo_save_draw.c
@@ -240,8 +240,11 @@ loopback_vertex_list(struct gl_context *ctx,
                                  list->vertex_store->bufferobj,
                                  MAP_INTERNAL);
 
+   unsigned buffer_offset =
+      aligned_vertex_buffer_offset(list) ? 0 : list->buffer_offset;
+
    vbo_loopback_vertex_list(ctx,
-                            (const GLfloat *)(buffer + list->buffer_offset),
+                            (const GLfloat *) (buffer + buffer_offset),
                             list->attrsz,
                             list->prims,
                             list->prim_count,

src/mesa/vbo/vbo_save_loopback.c
@@ -107,17 +107,17 @@ loopback_prim(struct gl_context *ctx,
    GLuint k;
 
    if (0)
-      printf("loopback prim %s(%s,%s) verts %d..%d\n",
+      printf("loopback prim %s(%s,%s) verts %d..%d  vsize %d\n",
              _mesa_lookup_prim_by_nr(prim->mode),
              prim->begin ? "begin" : "..",
              prim->end ? "end" : "..",
-             start, end);
+             start, end,
+             vertex_size);
 
    if (prim->begin) {
       CALL_Begin(GET_DISPATCH(), (prim->mode));
    }
    else {
-      assert(start == 0);
       start += wrap_count;
    }
 