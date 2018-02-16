yuq/mesa-lima

commit 198af27c679c0cabbb8747dd23fed5bd68bfdee2

wayland-egl: rename wayland-egl-{priv,backend}.h
In preparation to lifting the whole thing out as a separate library.

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Miguel A. Vico <mvicomoya@nvidia.com>

src/egl/drivers/dri2/egl_dri2.h
@@ -44,7 +44,7 @@
 
 #ifdef HAVE_WAYLAND_PLATFORM
 #include <wayland-client.h>
-#include "wayland-egl-priv.h"
+#include "wayland-egl-backend.h"
 /* forward declarations of protocol elements */
 struct zwp_linux_dmabuf_v1;
 #endif

src/egl/wayland/wayland-egl/Makefile.am
@@ -6,7 +6,7 @@ AM_CFLAGS = $(DEFINES) \
 	    $(WAYLAND_CLIENT_CFLAGS)
 
 lib_LTLIBRARIES = libwayland-egl.la
-noinst_HEADERS = wayland-egl-priv.h
+noinst_HEADERS = wayland-egl-backend.h
 libwayland_egl_la_SOURCES = wayland-egl.c
 libwayland_egl_la_LDFLAGS = \
 	-no-undefined \

src/egl/wayland/wayland-egl/wayland-egl-abi-check.c
@@ -23,7 +23,7 @@
 #include <stddef.h> // offsetof
 #include <stdio.h>  // printf
 
-#include "wayland-egl-priv.h" // Current struct wl_egl_window implementation
+#include "wayland-egl-backend.h" // Current struct wl_egl_window implementation
 
 /*
  * Following are previous implementations of wl_egl_window.
@@ -100,7 +100,7 @@ struct wl_egl_window_v3 {
 
 
 /* This program checks we keep a backwards-compatible struct wl_egl_window
- * definition whenever it is modified in wayland-egl-priv.h.
+ * definition whenever it is modified in wayland-egl-backend.h.
  *
  * The previous definition should be added above as a new struct
  * wl_egl_window_vN, and the appropriate checks should be added below

src/egl/wayland/wayland-egl/wayland-egl.c
@@ -32,7 +32,7 @@
 
 #include <wayland-client.h>
 #include "wayland-egl.h"
-#include "wayland-egl-priv.h"
+#include "wayland-egl-backend.h"
 
 WL_EGL_EXPORT void
 wl_egl_window_resize(struct wl_egl_window *egl_window,

projectceladon/external-mesa

commit db84d43dae6fa99397974bc38e53a91fe2e2b9ff

egl/wayland: Always use in-tree wayland-egl-backend.h
A recent patchset to Wayland[0] migrated Mesa's libwayland-egl backend
into Wayland itself, so implementations could provide backends. Mesa
still uses its own, and the two have already diverged[1].

The include from egl_dri2.h could pick up either the installed Wayland
wayland-egl-backend.h (with a 'driver_private' member), or the Mesa
internal wayland-egl-backend.h (with a 'private' member), failing the
build in the first instance.

Add an explicit directory prefix to the include, so we always get our
in-tree version.

[0]: https://patchwork.freedesktop.org/series/31663/
[1]: https://cgit.freedesktop.org/wayland/wayland/commit/?id=9fa60983b579

Signed-off-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=105103
Fixes: 198af27c679c ("wayland-egl: rename wayland-egl-{priv,backend}.h")

src/egl/drivers/dri2/egl_dri2.h
@@ -44,7 +44,7 @@
 
 #ifdef HAVE_WAYLAND_PLATFORM
 #include <wayland-client.h>
-#include "wayland-egl-backend.h"
+#include "wayland/wayland-egl/wayland-egl-backend.h"
 /* forward declarations of protocol elements */
 struct zwp_linux_dmabuf_v1;
 #endif