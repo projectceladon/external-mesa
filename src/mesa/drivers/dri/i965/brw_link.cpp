Igalia/mesa

i965: Link XFB varyings for SPIR-V shaders
(cherry picked from commit 7f3767c36cd4c9de01d96244fe31ede165f7660b)

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -270,6 +270,7 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
       }
 
       nir_link_assign_atomic_counter_resources(ctx, shProg);
+      nir_link_assign_xfb_resources(ctx, shProg);
    }
 
    /* Determine first and last stage. */

projectceladon/external-mesa

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -267,6 +267,7 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
          return false;
 
       gl_nir_link_assign_atomic_counter_resources(ctx, shProg);
+      gl_nir_link_assign_xfb_resources(ctx, shProg);
    }
 
    /* Determine first and last stage. */