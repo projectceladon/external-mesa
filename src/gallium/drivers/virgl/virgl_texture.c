yuq/mesa-lima

commit a8987b88ff1db4ac00720a9b56c4bc3aeb666537

virgl: add driver for virtio-gpu 3D (v2)
virgl is the 3D acceleration backend for the
virtio-gpu shipping with qemu.

The 3D acceleration is designed around gallium
and TGSI as the virtualisation layer. The backend
renderer translates the virgl interface into
OpenGL currently.

This is the initial import of the driver to mesa.

The kernel driver portions are lined up for drm-next.

Currently this driver supports up to GL3.3 and some
misc extensions if the host driver exposes it. It is
planned to iterate the virgl API to new GL levels
as mesa host drivers gain features.

v2: fix resource tracking across flushes to avoid
->bind hack in mapping.
consolidate mapping and waiting code for transfers.
use u_range for dirt tracking.
handle larger shaders in protocol.
include virtgpu_drm.h in mesa for now.
add translation layer for gallium tgsi to virgl tgsi.

Signed-off-by: Dave Airlie <airlied@redhat.com>

src/gallium/drivers/virgl/virgl_texture.c
@@ -1,0 +1,349 @@
+/*
+ * Copyright 2014, 2015 Red Hat.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "util/u_memory.h"
+#include "util/u_format.h"
+#include "virgl.h"
+#include "virgl_resource.h"
+#include "virgl_context.h"
+
+static void virgl_copy_region_with_blit(struct pipe_context *pipe,
+                                        struct pipe_resource *dst,
+                                        unsigned dst_level,
+                                        unsigned dstx, unsigned dsty, unsigned dstz,
+                                        struct pipe_resource *src,
+                                        unsigned src_level,
+                                        const struct pipe_box *src_box)
+{
+   struct pipe_blit_info blit;
+
+   memset(&blit, 0, sizeof(blit));
+   blit.src.resource = src;
+   blit.src.format = src->format;
+   blit.src.level = src_level;
+   blit.src.box = *src_box;
+   blit.dst.resource = dst;
+   blit.dst.format = dst->format;
+   blit.dst.level = dst_level;
+   blit.dst.box.x = dstx;
+   blit.dst.box.y = dsty;
+   blit.dst.box.z = dstz;
+   blit.dst.box.width = src_box->width;
+   blit.dst.box.height = src_box->height;
+   blit.dst.box.depth = src_box->depth;
+   blit.mask = util_format_get_mask(src->format) &
+      util_format_get_mask(dst->format);
+   blit.filter = PIPE_TEX_FILTER_NEAREST;
+
+   if (blit.mask) {
+      pipe->blit(pipe, &blit);
+   }
+}
+static void virgl_init_temp_resource_from_box(struct pipe_resource *res,
+                                              struct pipe_resource *orig,
+                                              const struct pipe_box *box,
+                                              unsigned level, unsigned flags)
+{
+   memset(res, 0, sizeof(*res));
+   res->format = orig->format;
+   res->width0 = box->width;
+   res->height0 = box->height;
+   res->depth0 = 1;
+   res->array_size = 1;
+   res->usage = PIPE_USAGE_STAGING;
+   res->flags = flags;
+
+   /* We must set the correct texture target and dimensions for a 3D box. */
+   if (box->depth > 1 && util_max_layer(orig, level) > 0)
+      res->target = orig->target;
+   else
+      res->target = PIPE_TEXTURE_2D;
+
+   switch (res->target) {
+   case PIPE_TEXTURE_1D_ARRAY:
+   case PIPE_TEXTURE_2D_ARRAY:
+   case PIPE_TEXTURE_CUBE_ARRAY:
+      res->array_size = box->depth;
+      break;
+   case PIPE_TEXTURE_3D:
+      res->depth0 = box->depth;
+      break;
+   default:
+      break;
+   }
+}
+
+static unsigned
+vrend_get_tex_image_offset(const struct virgl_texture *res,
+                           unsigned level, unsigned layer)
+{
+   const struct pipe_resource *pres = &res->base.u.b;
+   const unsigned hgt = u_minify(pres->height0, level);
+   const unsigned nblocksy = util_format_get_nblocksy(pres->format, hgt);
+   unsigned offset = res->level_offset[level];
+
+   if (pres->target == PIPE_TEXTURE_CUBE ||
+       pres->target == PIPE_TEXTURE_CUBE_ARRAY ||
+       pres->target == PIPE_TEXTURE_3D ||
+       pres->target == PIPE_TEXTURE_2D_ARRAY) {
+      offset += layer * nblocksy * res->stride[level];
+   }
+   else if (pres->target == PIPE_TEXTURE_1D_ARRAY) {
+      offset += layer * res->stride[level];
+   }
+   else {
+      assert(layer == 0);
+   }
+
+   return offset;
+}
+
+static void *virgl_texture_transfer_map(struct pipe_context *ctx,
+                                        struct pipe_resource *resource,
+                                        unsigned level,
+                                        unsigned usage,
+                                        const struct pipe_box *box,
+                                        struct pipe_transfer **transfer)
+{
+   struct virgl_context *vctx = (struct virgl_context *)ctx;
+   struct virgl_screen *vs = virgl_screen(ctx->screen);
+   struct virgl_texture *vtex = (struct virgl_texture *)resource;
+   enum pipe_format format = resource->format;
+   struct virgl_transfer *trans;
+   void *ptr;
+   boolean readback = TRUE;
+   uint32_t offset;
+   struct virgl_hw_res *hw_res;
+   const unsigned h = u_minify(vtex->base.u.b.height0, level);
+   const unsigned nblocksy = util_format_get_nblocksy(format, h);
+   bool is_depth = util_format_has_depth(util_format_description(resource->format));
+   uint32_t l_stride;
+   bool doflushwait;
+
+   doflushwait = virgl_res_needs_flush_wait(vctx, &vtex->base, usage);
+   if (doflushwait)
+      ctx->flush(ctx, NULL, 0);
+
+   trans = util_slab_alloc(&vctx->texture_transfer_pool);
+   if (trans == NULL)
+      return NULL;
+
+   trans->base.resource = resource;
+   trans->base.level = level;
+   trans->base.usage = usage;
+   trans->base.box = *box;
+   trans->base.stride = vtex->stride[level];
+   trans->base.layer_stride = trans->base.stride * nblocksy;
+
+   if (resource->target != PIPE_TEXTURE_3D &&
+       resource->target != PIPE_TEXTURE_CUBE &&
+       resource->target != PIPE_TEXTURE_1D_ARRAY &&
+       resource->target != PIPE_TEXTURE_2D_ARRAY &&
+       resource->target != PIPE_TEXTURE_CUBE_ARRAY)
+      l_stride = 0;
+   else
+      l_stride = trans->base.layer_stride;
+
+   if (is_depth && resource->nr_samples > 1) {
+      struct pipe_resource tmp_resource;
+      virgl_init_temp_resource_from_box(&tmp_resource, resource, box,
+                                        level, 0);
+
+      trans->resolve_tmp = (struct virgl_resource *)ctx->screen->resource_create(ctx->screen, &tmp_resource);
+
+      virgl_copy_region_with_blit(ctx, &trans->resolve_tmp->u.b, 0, 0, 0, 0, resource, level, box);
+      ctx->flush(ctx, NULL, 0);
+      /* we want to do a resolve blit into the temporary */
+      hw_res = trans->resolve_tmp->hw_res;
+      offset = 0;
+   } else {
+      offset = vrend_get_tex_image_offset(vtex, level, box->z);
+
+      offset += box->y / util_format_get_blockheight(format) * trans->base.stride +
+      box->x / util_format_get_blockwidth(format) * util_format_get_blocksize(format);
+      hw_res = vtex->base.hw_res;
+      trans->resolve_tmp = NULL;
+   }
+
+   readback = virgl_res_needs_readback(vctx, &vtex->base, usage);
+   if (readback)
+      vs->vws->transfer_get(vs->vws, hw_res, box, trans->base.stride, l_stride, offset, level);
+
+   if (doflushwait || readback)
+      vs->vws->resource_wait(vs->vws, vtex->base.hw_res);
+
+   ptr = vs->vws->resource_map(vs->vws, hw_res);
+   if (!ptr) {
+      return NULL;
+   }
+
+   trans->offset = offset;
+   *transfer = &trans->base;
+
+   return ptr + trans->offset;
+}
+
+static void virgl_texture_transfer_unmap(struct pipe_context *ctx,
+                                         struct pipe_transfer *transfer)
+{
+   struct virgl_context *vctx = (struct virgl_context *)ctx;
+   struct virgl_transfer *trans = (struct virgl_transfer *)transfer;
+   struct virgl_texture *vtex = (struct virgl_texture *)transfer->resource;
+   uint32_t l_stride;
+
+   if (transfer->resource->target != PIPE_TEXTURE_3D &&
+       transfer->resource->target != PIPE_TEXTURE_CUBE &&
+       transfer->resource->target != PIPE_TEXTURE_1D_ARRAY &&
+       transfer->resource->target != PIPE_TEXTURE_2D_ARRAY &&
+       transfer->resource->target != PIPE_TEXTURE_CUBE_ARRAY)
+      l_stride = 0;
+   else
+      l_stride = trans->base.layer_stride;
+
+   if (trans->base.usage & PIPE_TRANSFER_WRITE) {
+      if (!(transfer->usage & PIPE_TRANSFER_FLUSH_EXPLICIT)) {
+         struct virgl_screen *vs = virgl_screen(ctx->screen);
+         vtex->base.clean = FALSE;
+         vctx->num_transfers++;
+         vs->vws->transfer_put(vs->vws, vtex->base.hw_res,
+                               &transfer->box, trans->base.stride, l_stride, trans->offset, transfer->level);
+
+      }
+   }
+
+   if (trans->resolve_tmp)
+      pipe_resource_reference((struct pipe_resource **)&trans->resolve_tmp, NULL);
+
+   util_slab_free(&vctx->texture_transfer_pool, trans);
+}
+
+
+static boolean
+vrend_resource_layout(struct virgl_texture *res,
+                      uint32_t *total_size)
+{
+   struct pipe_resource *pt = &res->base.u.b;
+   unsigned level;
+   unsigned width = pt->width0;
+   unsigned height = pt->height0;
+   unsigned depth = pt->depth0;
+   unsigned buffer_size = 0;
+
+   for (level = 0; level <= pt->last_level; level++) {
+      unsigned slices;
+
+      if (pt->target == PIPE_TEXTURE_CUBE)
+         slices = 6;
+      else if (pt->target == PIPE_TEXTURE_3D)
+         slices = depth;
+      else
+         slices = pt->array_size;
+
+      res->stride[level] = util_format_get_stride(pt->format, width);
+      res->level_offset[level] = buffer_size;
+
+      buffer_size += (util_format_get_nblocksy(pt->format, height) *
+                      slices * res->stride[level]);
+
+      width = u_minify(width, 1);
+      height = u_minify(height, 1);
+      depth = u_minify(depth, 1);
+   }
+
+   if (pt->nr_samples <= 1)
+      *total_size = buffer_size;
+   else /* don't create guest backing store for MSAA */
+      *total_size = 0;
+   return TRUE;
+}
+
+static boolean virgl_texture_get_handle(struct pipe_screen *screen,
+                                         struct pipe_resource *ptex,
+                                         struct winsys_handle *whandle)
+{
+   struct virgl_screen *vs = virgl_screen(screen);
+   struct virgl_texture *vtex = (struct virgl_texture *)ptex;
+
+   return vs->vws->resource_get_handle(vs->vws, vtex->base.hw_res, vtex->stride[0], whandle);
+}
+
+static void virgl_texture_destroy(struct pipe_screen *screen,
+                                  struct pipe_resource *res)
+{
+   struct virgl_screen *vs = virgl_screen(screen);
+   struct virgl_texture *vtex = (struct virgl_texture *)res;
+   vs->vws->resource_unref(vs->vws, vtex->base.hw_res);
+   FREE(vtex);
+}
+
+static const struct u_resource_vtbl virgl_texture_vtbl =
+{
+   virgl_texture_get_handle,            /* get_handle */
+   virgl_texture_destroy,               /* resource_destroy */
+   virgl_texture_transfer_map,          /* transfer_map */
+   NULL,                                /* transfer_flush_region */
+   virgl_texture_transfer_unmap,        /* transfer_unmap */
+   NULL                                 /* transfer_inline_write */
+};
+
+struct pipe_resource *
+virgl_texture_from_handle(struct virgl_screen *vs,
+                          const struct pipe_resource *template,
+                          struct winsys_handle *whandle)
+{
+   struct virgl_texture *tex;
+   uint32_t size;
+
+   tex = CALLOC_STRUCT(virgl_texture);
+   tex->base.u.b = *template;
+   tex->base.u.b.screen = &vs->base;
+   pipe_reference_init(&tex->base.u.b.reference, 1);
+   tex->base.u.vtbl = &virgl_texture_vtbl;
+   vrend_resource_layout(tex, &size);
+
+   tex->base.hw_res = vs->vws->resource_create_from_handle(vs->vws, whandle);
+   return &tex->base.u.b;
+}
+
+struct pipe_resource *virgl_texture_create(struct virgl_screen *vs,
+                                           const struct pipe_resource *template)
+{
+   struct virgl_texture *tex;
+   uint32_t size;
+   unsigned vbind;
+
+   tex = CALLOC_STRUCT(virgl_texture);
+   tex->base.clean = TRUE;
+   tex->base.u.b = *template;
+   tex->base.u.b.screen = &vs->base;
+   pipe_reference_init(&tex->base.u.b.reference, 1);
+   tex->base.u.vtbl = &virgl_texture_vtbl;
+   vrend_resource_layout(tex, &size);
+
+   vbind = pipe_to_virgl_bind(template->bind);
+   tex->base.hw_res = vs->vws->resource_create(vs->vws, template->target, template->format, vbind, template->width0, template->height0, template->depth0, template->array_size, template->last_level, template->nr_samples, size);
+   if (!tex->base.hw_res) {
+      FREE(tex);
+      return NULL;
+   }
+   return &tex->base.u.b;
+}

configure.ac
@@ -866,7 +866,7 @@ GALLIUM_DRIVERS_DEFAULT="r300,r600,svga,swrast"
 AC_ARG_WITH([gallium-drivers],
     [AS_HELP_STRING([--with-gallium-drivers@<:@=DIRS...@:>@],
         [comma delimited Gallium drivers list, e.g.
-        "i915,ilo,nouveau,r300,r600,radeonsi,freedreno,svga,swrast,vc4"
+        "i915,ilo,nouveau,r300,r600,radeonsi,freedreno,svga,swrast,vc4,virgl"
         @<:@default=r300,r600,svga,swrast@:>@])],
     [with_gallium_drivers="$withval"],
     [with_gallium_drivers="$GALLIUM_DRIVERS_DEFAULT"])
@@ -2185,6 +2185,12 @@ if test -n "$with_gallium_drivers"; then
             PKG_CHECK_MODULES([SIMPENROSE], [simpenrose],
                               [USE_VC4_SIMULATOR=yes], [USE_VC4_SIMULATOR=no])
             ;;
+        xvirgl)
+            HAVE_GALLIUM_VIRGL=yes
+            gallium_require_drm "virgl"
+            gallium_require_drm_loader
+            require_egl_drm "virgl"
+            ;;
         *)
             AC_MSG_ERROR([Unknown Gallium driver: $driver])
             ;;
@@ -2256,6 +2262,7 @@ AM_CONDITIONAL(HAVE_GALLIUM_FREEDRENO, test "x$HAVE_GALLIUM_FREEDRENO" = xyes)
 AM_CONDITIONAL(HAVE_GALLIUM_SOFTPIPE, test "x$HAVE_GALLIUM_SOFTPIPE" = xyes)
 AM_CONDITIONAL(HAVE_GALLIUM_LLVMPIPE, test "x$HAVE_GALLIUM_LLVMPIPE" = xyes)
 AM_CONDITIONAL(HAVE_GALLIUM_VC4, test "x$HAVE_GALLIUM_VC4" = xyes)
+AM_CONDITIONAL(HAVE_GALLIUM_VIRGL, test "x$HAVE_GALLIUM_VIRGL" = xyes)
 
 AM_CONDITIONAL(HAVE_GALLIUM_STATIC_TARGETS, test "x$enable_shared_pipe_drivers" = xno)
 
@@ -2376,6 +2383,7 @@ AC_CONFIG_FILES([Makefile
 		src/gallium/drivers/svga/Makefile
 		src/gallium/drivers/trace/Makefile
 		src/gallium/drivers/vc4/Makefile
+		src/gallium/drivers/virgl/Makefile
 		src/gallium/state_trackers/clover/Makefile
 		src/gallium/state_trackers/dri/Makefile
 		src/gallium/state_trackers/glx/xlib/Makefile
@@ -2416,6 +2424,7 @@ AC_CONFIG_FILES([Makefile
 		src/gallium/winsys/sw/wrapper/Makefile
 		src/gallium/winsys/sw/xlib/Makefile
 		src/gallium/winsys/vc4/drm/Makefile
+		src/gallium/winsys/virgl/drm/Makefile
 		src/gbm/Makefile
 		src/gbm/main/gbm.pc
 		src/glsl/Makefile

src/gallium/Makefile.am
@@ -82,6 +82,11 @@ if HAVE_GALLIUM_VC4
 SUBDIRS += drivers/vc4 winsys/vc4/drm
 endif
 
+## virgl
+if HAVE_GALLIUM_VIRGL
+SUBDIRS += drivers/virgl winsys/virgl/drm
+endif
+
 ## the sw winsys'
 SUBDIRS += winsys/sw/null
 

src/gallium/auxiliary/target-helpers/inline_drm_helper.h
@@ -59,6 +59,11 @@
 #include "vc4/drm/vc4_drm_public.h"
 #endif
 
+#if GALLIUM_VIRGL
+#include "virgl/drm/virgl_drm_public.h"
+#include "virgl/virgl_public.h"
+#endif
+
 static char* driver_name = NULL;
 
 /* XXX: We need to teardown the winsys if *screen_create() fails. */
@@ -296,6 +301,33 @@ pipe_freedreno_create_screen(int fd)
 }
 #endif
 
+#if defined(GALLIUM_VIRGL)
+#if defined(DRI_TARGET)
+
+const __DRIextension **__driDriverGetExtensions_virtio_gpu(void);
+
+PUBLIC const __DRIextension **__driDriverGetExtensions_virtio_gpu(void)
+{
+   globalDriverAPI = &galliumdrm_driver_api;
+   return galliumdrm_driver_extensions;
+}
+#endif
+
+static struct pipe_screen *
+pipe_virgl_create_screen(int fd)
+{
+   struct virgl_winsys *vws;
+   struct pipe_screen *screen;
+
+   vws = virgl_drm_winsys_create(fd);
+   if (!vws)
+      return NULL;
+
+   screen = virgl_create_screen(vws);
+   return screen ? debug_screen_wrap(screen) : NULL;
+}
+#endif
+
 #if defined(GALLIUM_VC4)
 #if defined(DRI_TARGET)
 
@@ -385,6 +417,11 @@ dd_create_screen(int fd)
       return pipe_freedreno_create_screen(fd);
    else
 #endif
+#if defined(GALLIUM_VIRGL)
+   if ((strcmp(driver_name, "virtio_gpu") == 0))
+      return pipe_virgl_create_screen(fd);
+   else
+#endif
 #if defined(GALLIUM_VC4)
    if (strcmp(driver_name, "vc4") == 0)
       return pipe_vc4_create_screen(fd);
@@ -471,6 +508,11 @@ dd_configuration(enum drm_conf conf)
 #endif
 #if defined(GALLIUM_FREEDRENO)
    if ((strcmp(driver_name, "kgsl") == 0) || (strcmp(driver_name, "msm") == 0))
+      return configuration_query(conf);
+   else
+#endif
+#if defined(GALLIUM_VIRGL)
+   if ((strcmp(driver_name, "virtio_gpu") == 0))
       return configuration_query(conf);
    else
 #endif

src/gallium/drivers/virgl/Automake.inc
@@ -1,0 +1,10 @@
+if HAVE_GALLIUM_VIRGL
+
+TARGET_DRIVERS += virtio_gpu
+TARGET_CPPFLAGS += -DGALLIUM_VIRGL
+TARGET_LIB_DEPS += \
+	$(top_builddir)/src/gallium/drivers/virgl/libvirgl.la \
+	$(top_builddir)/src/gallium/winsys/virgl/drm/libvirgldrm.la
+	$(LIBDRM_LIBS)
+
+endif

src/gallium/drivers/virgl/Makefile.am
@@ -1,0 +1,43 @@
+# Copyright © 2014, 2015 Red Hat.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+include $(top_srcdir)/src/gallium/Automake.inc
+
+AM_CPPFLAGS = \
+	-I$(top_srcdir)/src/gallium/drivers \
+	-I$(top_srcdir)/src/gallium/winsys/virgl/drm \
+	-I$(top_srcdir)/include \
+	$(GALLIUM_CFLAGS) \
+	$(LIBDRM_CFLAGS)
+
+noinst_LTLIBRARIES = libvirgl.la
+
+libvirgl_la_SOURCES = \
+	virgl_screen.c \
+	virgl_resource.c \
+	virgl_buffer.c \
+	virgl_texture.c \
+	virgl_context.c \
+	virgl_encode.c \
+	virgl_query.c \
+	virgl_streamout.c \
+	virgl_tgsi.c

src/gallium/drivers/virgl/virgl.h
@@ -1,0 +1,51 @@
+/*
+ * Copyright 2014, 2015 Red Hat.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef VIRGL_H
+#define VIRGL_H
+
+#include "util/u_transfer.h"
+
+#include "../winsys/virgl/drm/virgl_hw.h"
+
+#include "virgl_winsys.h"
+#include "pipe/p_screen.h"
+struct virgl_screen {
+   struct pipe_screen base;
+   struct sw_winsys *winsys;
+   struct virgl_winsys *vws;
+
+   struct virgl_drm_caps caps;
+
+   uint32_t sub_ctx_id;
+};
+
+
+static inline struct virgl_screen *
+virgl_screen( struct pipe_screen *pipe )
+{
+   return (struct virgl_screen *)pipe;
+}
+
+#define VIRGL_MAP_BUFFER_ALIGNMENT 64
+
+#endif

src/gallium/drivers/virgl/virgl_buffer.c
@@ -1,0 +1,170 @@
+/*
+ * Copyright 2014, 2015 Red Hat.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "util/u_memory.h"
+#include "virgl_context.h"
+#include "virgl_resource.h"
+
+static void virgl_buffer_destroy(struct pipe_screen *screen,
+                                 struct pipe_resource *buf)
+{
+   struct virgl_screen *vs = virgl_screen(screen);
+   struct virgl_buffer *vbuf = virgl_buffer(buf);
+
+   util_range_destroy(&vbuf->valid_buffer_range);
+   vs->vws->resource_unref(vs->vws, vbuf->base.hw_res);
+   FREE(vbuf);
+}
+
+static void *virgl_buffer_transfer_map(struct pipe_context *ctx,
+                                       struct pipe_resource *resource,
+                                       unsigned level,
+                                       unsigned usage,
+                                       const struct pipe_box *box,
+                                       struct pipe_transfer **transfer)
+{
+   struct virgl_context *vctx = (struct virgl_context *)ctx;
+   struct virgl_screen *vs = virgl_screen(ctx->screen);
+   struct virgl_buffer *vbuf = virgl_buffer(resource);
+   struct virgl_transfer *trans;
+   void *ptr;
+   bool readback;
+   uint32_t offset;
+   bool doflushwait = false;
+
+   if ((usage & PIPE_TRANSFER_READ) && (vbuf->on_list == TRUE))
+      doflushwait = true;
+   else
+      doflushwait = virgl_res_needs_flush_wait(vctx, &vbuf->base, usage);
+
+   if (doflushwait)
+      ctx->flush(ctx, NULL, 0);
+
+   trans = util_slab_alloc(&vctx->texture_transfer_pool);
+   if (trans == NULL)
+      return NULL;
+
+   trans->base.resource = resource;
+   trans->base.level = level;
+   trans->base.usage = usage;
+   trans->base.box = *box;
+   trans->base.stride = 0;
+   trans->base.layer_stride = 0;
+
+   offset = box->x;
+
+   readback = virgl_res_needs_readback(vctx, &vbuf->base, usage);
+   if (readback)
+      vs->vws->transfer_get(vs->vws, vbuf->base.hw_res, box, trans->base.stride, trans->base.layer_stride, offset, level);
+
+   if (!(usage & PIPE_TRANSFER_UNSYNCHRONIZED))
+      doflushwait = true;
+
+   if (doflushwait || readback)
+      vs->vws->resource_wait(vs->vws, vbuf->base.hw_res);
+
+   ptr = vs->vws->resource_map(vs->vws, vbuf->base.hw_res);
+   if (!ptr) {
+      return NULL;
+   }
+
+   trans->offset = offset;
+   *transfer = &trans->base;
+
+   return ptr + trans->offset;
+}
+
+static void virgl_buffer_transfer_unmap(struct pipe_context *ctx,
+                                        struct pipe_transfer *transfer)
+{
+   struct virgl_context *vctx = (struct virgl_context *)ctx;
+   struct virgl_transfer *trans = (struct virgl_transfer *)transfer;
+   struct virgl_buffer *vbuf = virgl_buffer(transfer->resource);
+
+   if (trans->base.usage & PIPE_TRANSFER_WRITE) {
+      if (!(transfer->usage & PIPE_TRANSFER_FLUSH_EXPLICIT)) {
+         struct virgl_screen *vs = virgl_screen(ctx->screen);
+         vbuf->base.clean = FALSE;
+         vctx->num_transfers++;
+         vs->vws->transfer_put(vs->vws, vbuf->base.hw_res,
+                               &transfer->box, trans->base.stride, trans->base.layer_stride, trans->offset, transfer->level);
+
+      }
+   }
+
+   util_slab_free(&vctx->texture_transfer_pool, trans);
+}
+
+static void virgl_buffer_transfer_flush_region(struct pipe_context *ctx,
+                                               struct pipe_transfer *transfer,
+                                               const struct pipe_box *box)
+{
+   struct virgl_context *vctx = (struct virgl_context *)ctx;
+   struct virgl_buffer *vbuf = virgl_buffer(transfer->resource);
+
+   if (!vbuf->on_list) {
+       struct pipe_resource *res = NULL;
+
+       list_addtail(&vbuf->flush_list, &vctx->to_flush_bufs);
+       vbuf->on_list = TRUE;
+       pipe_resource_reference(&res, &vbuf->base.u.b);
+   }
+
+   util_range_add(&vbuf->valid_buffer_range, transfer->box.x + box->x,
+                  transfer->box.x + box->x + box->width);
+
+   vbuf->base.clean = FALSE;
+}
+
+static const struct u_resource_vtbl virgl_buffer_vtbl =
+{
+   u_default_resource_get_handle,            /* get_handle */
+   virgl_buffer_destroy,                     /* resource_destroy */
+   virgl_buffer_transfer_map,                /* transfer_map */
+   virgl_buffer_transfer_flush_region,       /* transfer_flush_region */
+   virgl_buffer_transfer_unmap,              /* transfer_unmap */
+   virgl_transfer_inline_write               /* transfer_inline_write */
+};
+
+struct pipe_resource *virgl_buffer_create(struct virgl_screen *vs,
+                                          const struct pipe_resource *template)
+{
+   struct virgl_buffer *buf;
+   uint32_t size;
+   uint32_t vbind;
+   buf = CALLOC_STRUCT(virgl_buffer);
+   buf->base.clean = TRUE;
+   buf->base.u.b = *template;
+   buf->base.u.b.screen = &vs->base;
+   buf->base.u.vtbl = &virgl_buffer_vtbl;
+   pipe_reference_init(&buf->base.u.b.reference, 1);
+   util_range_init(&buf->valid_buffer_range);
+
+   vbind = pipe_to_virgl_bind(template->bind);
+   size = template->width0;
+
+   buf->base.hw_res = vs->vws->resource_create(vs->vws, template->target, template->format, vbind, template->width0, 1, 1, 1, 0, 0, size);
+
+   util_range_set_empty(&buf->valid_buffer_range);
+   return &buf->base.u.b;
+}

projectceladon/external-mesa

commit 2929b91371849d1700b221142b0d4961435d8e12

virgl: adjust strides when mapping temp-resources
When we're mapping temp-resources, we clip the resource to the
transfer-box, which means the stride might not be correct any more.

So let's update the stride from the temp-resource, and recompute the
layer-stride.

This fixes crashes when running dEQP with --deqp-gl-config-name=rgba8888d24s8ms4

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Fixes: a8987b88ff1 "virgl: add driver for virtio-gpu 3D (v2)"
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/gallium/drivers/virgl/virgl_texture.c
@@ -176,6 +176,8 @@ static void *virgl_texture_transfer_map(struct pipe_context *ctx,
       /* we want to do a resolve blit into the temporary */
       hw_res = trans->resolve_tmp->hw_res;
       offset = 0;
+      trans->base.stride = ((struct virgl_texture*)trans->resolve_tmp)->stride[level];
+      trans->base.layer_stride = trans->base.stride * nblocksy;
    } else {
       offset = vrend_get_tex_image_offset(vtex, level, box->z);
 