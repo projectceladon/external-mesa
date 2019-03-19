afa8f44821ec931d058fe550189beb776a28f4ba
renchenglei <chenglei.ren@intel.com>
2019-03-19 15:57:20 +0800

    WA the state cache flush issue
    On Gordenpeak platform, this state flush function have problem.
    It will cause GPU read some garbage data sometime.
    So add time wait after cache flush.
    After stress test, It can keep GPU read the right state data now.
    Will continue to optimize this solution.
    
    Signed-off-by: Yang Shi <yang.a.shi@intel.com>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -1037,7 +1037,10 @@ struct anv_device {
     pthread_mutex_t                             mutex;
     pthread_cond_t                              queue_submit;
     bool                                        _lost;
+    bool                                        needwait;
 };
+#define WAIT_DETECT_MAX_LEN 100
+
 
 static inline struct anv_state_pool *
 anv_binding_table_pool(struct anv_device *device)
@@ -1078,6 +1081,8 @@ anv_state_flush(struct anv_device *device, struct anv_state state)
       return;
 
    gen_flush_range(state.map, state.alloc_size);
+   if(device->needwait)
+      usleep(8000);
 }
 
 void anv_device_init_blorp(struct anv_device *device);
