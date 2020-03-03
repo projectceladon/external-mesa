Grima04/mesa-turnip-kgsl

radeonsi: disable SDMA on gfx8 to fix corruption on RX 580
Closes: #1399
Closes: #1889

Cc: 19.2 19.3 <mesa-stable@lists.freedesktop.org>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Reviewed-By: Timur Kristóf <timur.kristof@gmail.com>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -489,6 +489,11 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen,
 
 	if (sscreen->info.num_rings[RING_DMA] &&
 	    !(sscreen->debug_flags & DBG(NO_SDMA)) &&
+	    /* SDMA causes corruption on RX 580:
+	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1399
+	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1889
+	     */
+	    (sctx->chip_class != GFX8 || sscreen->debug_flags & DBG(FORCE_SDMA)) &&
 	    /* SDMA timeouts sometimes on gfx10 so disable it for now. See:
 	     *    https://bugs.freedesktop.org/show_bug.cgi?id=111481
 	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1907

projectceladon/external-mesa

(cherry picked from commit e5167a9276de1f383888714b41d3a9be2b9c1da9)
Conflicts:
	src/gallium/drivers/radeonsi/si_pipe.c

src/gallium/drivers/radeonsi/si_pipe.c
@@ -485,6 +485,11 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen,
 
 	if (sscreen->info.num_sdma_rings &&
 	    !(sscreen->debug_flags & DBG(NO_ASYNC_DMA)) &&
+	    /* SDMA causes corruption on RX 580:
+	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1399
+	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1889
+	     */
+	    (sctx->chip_class != GFX8 || sscreen->debug_flags & DBG(FORCE_DMA)) &&
 	    /* SDMA timeouts sometimes on gfx10 so disable it for now. See:
 	     *    https://bugs.freedesktop.org/show_bug.cgi?id=111481
 	     *    https://gitlab.freedesktop.org/mesa/mesa/issues/1907