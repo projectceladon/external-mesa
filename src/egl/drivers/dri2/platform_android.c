5ee571dc35b824a9fe10dfe94c7727cf5a111ffd
renchenglei <chenglei.ren@intel.com>
2020-08-17 21:56:36 +0800

    INTERNAL: Fix 4 CTS Media issues
    Sometimes, when min_buffer_count is equal to max_buffer_count(1),
    the buffer count will still be 1. And we will not conside 1 more
    dequeue buffer. This will cause "setBufferCount" error in framework.
    
    Tests: Fix CTS Media issues android.media.cts.HeifWriterTest
    
    Signed-off-by: Chenglei Ren <chenglei.ren@intel.com>
    Tracked-On: OAM-91605

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -741,8 +741,11 @@ droid_create_surface(_EGLDisplay *disp, EGLint type, _EGLConfig *conf,
       }
 
       /* Clamp preferred between minimum (min undequeued + 1 dequeued)
-       * and maximum.
+       * and maximum. And we also need confirm min_buffer_count less
+       * than max_buffer_count
        */
+      if (min_buffer_count == max_buffer_count)
+         max_buffer_count++;
       buffer_count = CLAMP(preferred_buffer_count, min_buffer_count + 1,
                            max_buffer_count);
 
