44ed50f3b8e540ffd73e7431c43526d8dee1515c
HeYue <yue.he@intel.com>
2024-04-30 16:36:37 +0800

    Refine the dGPU render
    Check the dGPU through whether has local memory.
    Calculate the i915 render node number for better
    compatibility.
    The GLES open one available render node, vulkan
    open all available render nodes.
    Enalbe the vulkan dGPU render.
    
    Tracked-On: OAM-122333
    Signed-off-by: HeYue <yue.he@intel.com>

--- src/intel/common/intel_check.h
+++ src/intel/common/intel_check.h
@@ -1,11 +1,15 @@
 #ifndef _CHECK_DGPU_H
 #define _CHECK_DGPU_H
 #include <stdbool.h>
+#include "util/libdrm.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 bool intel_is_dgpu_render(void);
 bool intel_lower_ctx_priority(void);
+bool is_dgpu(int id);
+bool has_local_mem(int fd);
+int get_intel_node_num(void);
 #ifdef __cplusplus
 }
 #endif
