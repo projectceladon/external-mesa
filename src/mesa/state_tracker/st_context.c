d68f8674e470474c79423ff59360a5ab6151d714
shenghualin <shenghua.lin@intel.com>
2024-05-29 11:33:35 +0800

    iris/anv: Disable hw acceleration for astc 1.on MTL platform 2.CTS case
    Disable HW accelaration on MTL platform.
    On other platforms, only disable hw accelaration when running CTS.
    Will enable again when it works well on MTL.
    
    Tracked-On: OAM-116833
    Signed-off-by: Lin, Shenghua <shenghua.lin@intel.com>

--- src/mesa/state_tracker/st_context.c
+++ src/mesa/state_tracker/st_context.c
@@ -547,6 +547,7 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
                                                    PIPE_TEXTURE_2D, 0, 0,
                                                    PIPE_BIND_SAMPLER_VIEW);
    st->transcode_astc = options->transcode_astc &&
+                        screen->get_param(screen, PIPE_CAP_TRANSCODE_ASTC_WORK_WELL) &&
                         screen->is_format_supported(screen, PIPE_FORMAT_DXT5_SRGBA,
                                                     PIPE_TEXTURE_2D, 0, 0,
                                                     PIPE_BIND_SAMPLER_VIEW) &&
