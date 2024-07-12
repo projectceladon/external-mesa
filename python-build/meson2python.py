df51e2cd77776a9cf44ecdba7a234fd52ac311f1
C Stout <cstout@google.com>
2024-07-11 18:56:37 -0700

    python-build: support format strings
    BUG=342026860
    TEST=python-build
    
    Change-Id: I18e29ce1dff7a28b94e691a85233c1059652b1a4

--- python-build/meson2python.py
+++ python-build/meson2python.py
@@ -102,8 +102,8 @@ meson_grammar = r"""
     NEWLINE: ( / *\r?\n/ | COMMENT )+
     COMMENT: / *\#.*\n/
     SLASH: /\//
-    STRING_SIMPLE_VALUE: /'(.*\\')*.*?'/
-    STRING_MULTILINE_VALUE: /'''.*?'''/s
+    STRING_SIMPLE_VALUE: /f?'(.*\\')*.*?'/
+    STRING_MULTILINE_VALUE: /f?'''.*?'''/s
     TRUE: /true/
 
     %import common.WS
@@ -316,6 +316,8 @@ class TreeToCode(Interpreter):
     assert len(tree.children) == 1
     string = tree.children[0]
     string = re.sub(r'(@[0-9]@)', r'{}', string)
+    if string.startswith('f'):
+      string = re.sub(r'(@(.+)@)', r'{\g<2>}', string)
     return string
 
   def __default__(self, tree):
