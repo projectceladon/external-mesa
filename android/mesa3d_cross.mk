390153768a1bc2b87c34d388812450908dbee1a4
ZhuChenyanX <zhucx@intel.com>
2024-12-12 08:36:12 +0000

    Fix CtsGpuProfilingDataTest fail
    Rename the perfetto gpu data producer name from libpps-producer
     to libgpudataproducer to align with AGI/cts.
    
    To pass the test, it also need following cmd:
    1, setenforce 0
    2, sysctl dev.i915.perf_stream_paranoid=0
       sysctl dev.i915_ag.perf_stream_paranoid=0
    When in vm mode, at least 1 gpu needs to be passthoughed.
    SRIOV mode has no metrics info provided under dir:
      /sys/dev/char/%d:%d/device/drm
    
    Tracked-On: OAM-128560
    Signed-off-by: ZhuChenyanX <zhucx@intel.com>

--- android/mesa3d_cross.mk
+++ android/mesa3d_cross.mk
@@ -96,6 +96,7 @@ MESON_GEN_NINJA := \
 	-Dlmsensors=disabled                                                         \
 	-Dandroid-libbacktrace=disabled                                              \
 	-Dperfetto=true                                                              \
+	-Ddatasources=auto                                                           \
 	$(BOARD_MESA3D_MESON_ARGS)                                                   \
 
 MESON_BUILD := PATH=/usr/bin:/bin:/sbin:$$PATH ninja -C $(MESON_OUT_DIR)/build
