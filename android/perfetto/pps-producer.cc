74d2263df58a4d3e795c88eeb08f56fe7dff0a01
ZhuChenyanX <zhucx@intel.com>
2024-12-12 08:36:12 +0000

    Fix CtsGpuProfilingDataTest fail
    Rename the perfetto gpu data producer name from libpps-producer
     to libgpudataproducer to align with AGI/cts.
    
    To pass the test, it also need following cmd:
    1, setenforce 0
    2, sysctl dev.i915.perf_stream_paranoid=0
       sysctl dev.i915_ag.perf_stream_paranoid=0
    When in vm mode, at least 1 gpu needs to be passthoughed.
    SRIOV mode has no metrics info provided under dir:
      /sys/dev/char/%d:%d/device/drm
    
    Tracked-On: OAM-128278
    Signed-off-by: ZhuChenyanX <zhucx@intel.com>

--- android/perfetto/pps-producer.cc
+++ android/perfetto/pps-producer.cc
@@ -1,5 +1,30 @@
-extern int pps_main(int argc, const char **argv);
+#include <cstdio>
+#include <dlfcn.h>
+
+typedef void (*pps_main_fn_t)(int, const char **);
+
+#define PPS_LIB "/vendor/lib64/libgpudataproducer.so"
 
 int main(int argc, const char **argv) {
-    return pps_main(argc, argv);
+  char *error;
+
+  void *handle = dlopen(PPS_LIB, RTLD_GLOBAL);
+  if ((error = dlerror()) != nullptr || handle == nullptr) {
+    fprintf(stdout, "Error loading lib: %s\n", error);
+    return -1;
+  }
+
+  pps_main_fn_t ppsFn = (pps_main_fn_t)dlsym(handle, "pps_main");
+  if (((error = dlerror()) != nullptr) || (ppsFn == nullptr)) {
+    fprintf(stdout, "Error looking for pps_main symbol: %s\n", error);
+    dlclose(handle);
+    return -1;
+  }
+
+  fprintf(stdout, "start call pps_main\n");
+  (*ppsFn)(argc, argv);
+  fprintf(stdout, "end call pps_main\n");
+
+  dlclose(handle);
+  return 0;
 }
